07efdc47ce625951d07e502fc20ed56e109c412c
Josh Cheney <joshcheney125@gmail.com>
2021-11-05 16:03:41 +0000

    working create and show all

---
+++ src/showAlltoDos.js
@@ -0,0 +1,10 @@
+// - Show all todo items
+
+// Methods                      Inputs                     Scenario                                            Outputs
+// showAllToDos                 objects                    if toDos exist show all                             toDoList[toDos]
+
+function showAllToDos(toDo, status) {
+    return toDo
+}
+
+module.exports = showAllToDos
