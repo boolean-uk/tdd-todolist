069d1b5f065b89973543d7c40a2196bdd85bd3c0
Anica Rahman <anicarahman2009@yahoo.it>
2022-03-21 15:47:06 +0000

    First requirement passed

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,22 @@
 class TodoList {
+  todos = [];
 
+  create(text) {
+    const todoToCreate = {
+      id: this.todos.length + 1,
+      text: text,
+      status: "incomplete",
+    };
+
+    this.todos.push(todoToCreate);
+
+    return todoToCreate;
+  }
 }
 
-module.exports = TodoList
+module.exports = TodoList;
+
+// Add a function/method (getalltodo) to your domain model.
+// Add a test for the new method
+// Run the tests (some will fail)
+// Write/Update the code to pass the test.
