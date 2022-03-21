c65ccb68955d859f58fafdb6d53ce169d850a15a
Dan <dannorthcott8@gmail.com>
2022-03-21 18:11:55 +0000

    initial commit

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,42 @@
 class TodoList {
+  constructor() {
+    this.toDoArray = [];
+  }
 
+  create(text) {
+    const todoItem = {
+      id: this.toDoArray.length + 1,
+      text: text,
+      status: "incomplete",
+    };
+
+    this.toDoArray.push(todoItem);
+
+    return todoItem;
+  }
+
+  getAll() {
+    return this.toDoArray;
+  }
+
+  setCompleteByID(ID) {
+    this.toDoArray.forEach((toDoItem) => {
+      if (toDoItem.id === ID) {
+        toDoItem.status = "complete";
+      }
+    });
+
+    return this.toDoArray;
+    // alternative method using .find()
+    // let item = this.toDoArray.find((item) => item.id === ID);
+    // if (item === undefined) throw new Error("cant find item");
+    // item.status = "complete";
+    // return item;
+  }
 }
 
-module.exports = TodoList
+// const todoList = new TodoList();
+// todoList.create("test");
+// todoList.getAll();
+
+module.exports = TodoList;
