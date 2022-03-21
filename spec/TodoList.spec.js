137b04df864aa85d96f76a61fd9a8674c7729df9
Chris Hyde <c_hyde333@hotmail.com>
2022-03-21 17:57:28 +0000

    Three tests done

--- spec/TodoList.spec.js
+++ spec/TodoList.spec.js
@@ -14,4 +14,47 @@ describe("TodoList", () => {
     // verify
     expect(result).toEqual(expected)
   })
+
+  it("creates a different todo item", () => {
+  // set up
+    const todoList = new TodoList()
+    const expected = {
+      id: 1,
+      text: "turn the air conditioning on!",
+      status: "incomplete"
+    }
+    // execute
+    const result = todoList.create("turn the air conditioning on!")
+    // verify
+    expect(result).toEqual(expected)
+  })
+
+  it("should increment the id", () => {
+    // set up
+    const todoList = new TodoList();
+    const firstTodo = {
+       id: 1,
+      text: "turn the heating on!",
+      status: "incomplete"
+    }
+    const secondTodo = {
+        id: 2,
+      text: "turn the air conditioning on!",
+      status: "incomplete"
+    }
+    // execute 
+    todoList.create("turn the heating on!")
+    const result = todoList.create("turn the air conditioning on!")
+
+    // verify 
+    expect(result).toEqual(secondTodo)
+  })
+
+  it("show all completed todos", () => {
+    //set up
+     id:  
+     text: 
+     status: "complete"
+  }
+  expect(result).toEqual(expected)
 })
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,23 @@
 class TodoList {
+    todos = []
 
+
+create(text) {
+    const todoToCreate = {
+      id: this.todos.length + 1,
+      text: text,
+      status: "incomplete"
+    };
+
+    this.todos.push(todoToCreate)
+
+
+    return todoToCreate
+}
 }
 
 module.exports = TodoList
+
+
+//  console.log("todoToCreate", todoToCreate) 
+// console.log("todo array", this.todos)
