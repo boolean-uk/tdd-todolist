e5021e57509a2002f8c5fcf3a101a699aa584d44
Tibor22 <tothtibitravel@gmail.com>
2022-03-21 17:24:13 +0000

    done testing

--- domain-model.md
+++ domain-model.md
@@ -1,1 +1,19 @@
 // Add your domain model below
+
+## Objects | Properties | Messages | Output
+
+item | ID@number  
+item | text@String
+item | status@String
+item | isComplete@Boolean
+
+---
+
+todoList | item@Obj | create(text@String) | item@Obj
+toDoList| Items@Array[item] | create(text@String) |
+todoList | | getCompletes(items@Array) | items@array
+todoList | | getInCompletes(items@Array) | items@array
+todoList | | getList() | Items@Array
+todoList | | searchById(id) | item@Obj || @String
+todoList | | removeItem(id) | items@Array
+todoList | | setItemComplete(id) | items@Obj
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,48 @@
 class TodoList {
+  items = [];
 
+  create(text) {
+    let item = {
+      id: this.items.length + 1,
+      text,
+      status: "incomplete",
+    };
+    this.items.push(item);
+    if (this.items.length === 1) return item;
+    return this.items;
+  }
+
+  setItemComplete(id) {
+    this.items.forEach((item) => {
+      if (item.id === id) {
+        item.status = "complete";
+      }
+    });
+    return this.items;
+  }
+
+  getIncompletes() {
+    let incompletes = this.items.filter((item) => item.status === "incomplete");
+    return incompletes;
+  }
+  getCompletes() {
+    let completes = this.items.filter((item) => item.status === "complete");
+    return completes;
+  }
+
+  searchById(id) {
+    const searchedId = this.items.find((item) => item.id === id);
+    return searchedId || "It doesn't exist";
+  }
+
+  removeItem(id) {
+    this.items.forEach((item, i) => {
+      if (item.id === id) {
+        this.items.splice(i, 1);
+      }
+    });
+    return this.items;
+  }
 }
 
-module.exports = TodoList
+module.exports = TodoList;
