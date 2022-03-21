407046a090e8d410fa7cb2ea378f8cf99f8a1d63
David <czdavid93@gmail.com>
2022-03-21 18:58:03 +0100

    domain model and first tests done

--- spec/TodoList.spec.js
+++ spec/TodoList.spec.js
@@ -14,4 +14,41 @@ describe("TodoList", () => {
     // verify
     expect(result).toEqual(expected)
   })
+
+  it("creates a second todo item with a differt description", () => {
+    // set up
+    const todoList = new TodoList()
+    const expected = {
+      id: 1,
+      text: "open the window",
+      status: "incomplete"
+    }
+    // execute
+    const result = todoList.create("open the window")
+    // verify
+    expect(result).toEqual(expected)
+  })
+  
+
+  it("this todo should increment the ID", () => {
+    // set up
+    const todoList = new TodoList()
+    const firstToDO = {
+      id: 1,
+      text: "turn the heating on!",
+      status: "incomplete"
+    }
+    const secondToDo = {
+      id: 2,
+      text: "second thing to do",
+      status: "incomplete"
+    }
+    // execute
+    todoList.create("first thing to do")
+    const result = todoList.create("second thing to do")
+    // verify
+    expect(result).toEqual(secondToDo)
+  })
+
+
 })
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,20 @@
 class TodoList {
+    constructor(){
+        this.idCount = 1
+    }
+
+  create(description) {
+    const todo = {
+      id: this.idCount,
+      text: description,
+      status: "incomplete",
+    };
+    this.idCount += 1
+    console.log(todo)
+    return todo
+  }
+
 
 }
 
-module.exports = TodoList
+module.exports = TodoList;
