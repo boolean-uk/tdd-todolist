f949611e13b37cf587ad0b30ee39d246c4f908a9
Paulo Barata <pbara7a@gmail.com>
2022-03-21 18:03:49 +0000

    Instructions for the user and a command to exit the program.

--- src/TodoList.js
+++ src/TodoList.js
@@ -65,5 +65,31 @@ class TodoList {
         return `There is no todo with id: ${id}.`
     }
 }
+const prompt = require('prompt-sync')()
+
+function instructions () {
+    console.log('INSTRUCTIONS:')
+    console.log('Type "create" followed by the task to add a new todo.')
+    console.log('Type "all" to get the list of todos.')
+    console.log('Type "complete" to get the list of completed todos.')
+    console.log('Type "incomplete" to get the list of incompleted todos.')
+    console.log('Type "todo" followed by an id to get a specific todo.')
+    console.log('Type "done" followed by an id to set a specific todo complete.')
+    console.log('Type "delete" followed by an id to remove a specific todo from the list.')
+    console.log('Type "quit" to quit the program at any time.')
+}
+
+const name = prompt('What is your name?')
+console.log(`Hey there ${name}`)
+instructions()
+
+
+let quit = false
+while (!quit) {
+    const input = prompt('What do you want to do?').toLowerCase()
+    if (input === 'quit') {
+        quit = true
+    }
+}
 
 module.exports = TodoList
