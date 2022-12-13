35dc9a1e2142c7b40700eec4d05416c04e0cfcb2
Luciano Simoni <lucianosimonipersonal@gmail.com>
2022-12-13 14:22:58 +0000

    push check working

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -12,14 +12,15 @@ describe('Test of TodoList class', () => {
     expect(todoList.todos.length).toBe(0)
   })
   it('expects newId of newTodo() to always be new', () => {
-    console.log(todoList.newTodo('first todo'))
-    console.log(todoList.newTodo('second todo'))
+    todoList.newTodo('first todo')
+    todoList.newTodo('second todo')
     const secondTodo = todoList.todos[1]
     expect(secondTodo.id).toBe(2)
   })
 
   it('expects newTodo to push to this.todos', () => {
     todoList.newTodo('do something')
-    expect(todoList.todos[0]).toEqual({ text: 'do something' })
+    todoList.newTodo('do second something')
+    expect(todoList.todos.length).toBe(2)
   })
 })
