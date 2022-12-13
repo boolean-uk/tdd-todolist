c012a49c9cd922ed01e463c3ad087b68e8cb04cf
Dave <Daverevell491@gmail.com>
2022-12-13 16:26:08 +0000

    First test failed

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,1 +1,18 @@
+const TodoList = require('../src/todolist')
 
+describe('todolist', () => {
+  // don't need yet, just wanted to get it down ready
+  // let todoList
+  // beforeEach(() => {
+  //   todoList = new TodoList()
+  // })
+
+  it('creates a todo item', () => {
+    const expected = {
+      text: 'exercise',
+      id: 1,
+      status: 'incomplete'
+    }
+    expected(result).ToEqual(expected)
+  })
+})
