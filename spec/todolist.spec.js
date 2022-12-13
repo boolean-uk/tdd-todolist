892bb3e5a188ff08cee75f2b71a5d42dcaa3730d
Joel Watkins <eck087@yahoo.co.uk>
2022-12-13 15:19:21 +0000

    tests for three functions added

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,1 +1,35 @@
+const {
+  createTodo,
+  todos,
+  getAllTodos,
+  setComplete
+} = require('../src/todolist')
 
+describe('todos', () => {
+  beforeEach(() => {
+    todos.splice(0, todos.length)
+  })
+
+  it('createToDo should add an object to todos', () => {
+    createTodo('Watch world cup')
+
+    expect(todos).toEqual([{ task: 'Watch world cup', complete: false, id: 1 }])
+  })
+
+  it('getAllTodos should return todos list', () => {
+    const allTodos = getAllTodos()
+
+    expect(allTodos).toEqual(todos)
+  })
+
+  it('setComplete edits a todo item to change complete from false to true', () => {
+    createTodo('Work on React project')
+    setComplete(1)
+
+    expect(todos[0]).toEqual({
+      task: 'Work on React project',
+      id: 1,
+      complete: true
+    })
+  })
+})
--- src/todolist.js
+++ src/todolist.js
@@ -1,1 +1,23 @@
+const todos = []
 
+const createTodo = (todoText) => {
+  const todoItem = { task: todoText, complete: false, id: todos.length + 1 }
+
+  todos.push(todoItem)
+}
+
+const getAllTodos = () => {
+  return todos
+}
+
+const setComplete = (id) => {
+  const found = todos.find((todo) => todo.id === id)
+  if (!found) return
+
+  found.complete = true
+}
+
+// createTodo('Watch world cup')
+// setComplete(1)
+
+module.exports = { createTodo, todos, getAllTodos, setComplete }
