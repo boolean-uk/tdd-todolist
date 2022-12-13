6b9319e30d1fedc0274bc0fe11d49dd7c4af7185
Luciano Simoni <lucianosimonipersonal@gmail.com>
2022-12-13 13:44:38 +0000

    push new todo

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,1 +1,13 @@
+const { TodoList } = require('../src/todolist')
 
+describe('Test of TodoList class', () => {
+  let todoList
+  beforeEach(() => {
+    todoList = new TodoList()
+  })
+
+  it('adds new Todo', () => {
+    todoList.newTodo('do something')
+    expect(todoList.todos[0]).toEqual({ text: 'do something' })
+  })
+})
--- src/todolist.js
+++ src/todolist.js
@@ -1,1 +1,17 @@
+class TodoList {
+  constructor() {
+    this.todos = []
+  }
 
+  newTodo(text) {
+    const newTodo = {
+      text: null
+    }
+
+    this.todos.push(newTodo)
+  }
+}
+
+module.exports = {
+  TodoList
+}
