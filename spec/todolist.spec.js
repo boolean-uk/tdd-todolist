e632944317642b50ba0de96c535f29507272f111
Enky <enkh6at@yahoo.com>
2022-12-19 12:36:53 +0000

    half way

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,5 +1,35 @@
-const { addToDoList } = require('../src/todolist.js')
+const { TodoList } = require('../src/todolist')
 
-describe("Add item to todo list", ()=>{
-    console.log(addToDoList("washup"))
+describe('todoList', () => {
+  let todoList 
+  beforeEach(()=>{
+    todoList = new TodoList()
+  })
+
+  it('should define todoList', () => {
+    expect(todoList).toBeDefined()
+  })
+
+  it('Create a todo item that has an ID, text description, and starts off incomplete', () => {
+    const todoItem = 'Do something'
+    expect(todoList.create(todoItem)).toEqual({
+      id: 1,
+      description: 'Do something',
+      status: 'incomplete'
+    })
+  })
+
+  it('get all todo items', ()=>{
+    todoList.create('google code')
+    todoList.create('to code facebook')
+    expect(todoList.getAlltodo()).toHaveSize(2)
+  })
+
+  it('completed todo by its id', ()=>{
+    todoList.create('be happy')
+    expect(todoList.completedTodo()).toEqual([
+      { id:1, description: 'be happy', status: true}
+    ])
+  })
 })
+
--- src/todolist.js
+++ src/todolist.js
@@ -1,15 +1,62 @@
-const toDos = []
-const item = {id: toDos.length +1, 
-    completed: false,
-    text: ""
-}
+class TodoList {
+  constructor() {
+    this.id = 0
+    this.todos = []
+    // this.complete = false
+  }
+  // - Create a todo item that has an ID,
+  // text description, and starts off incomplete
+  create(txt) {
+    this.id++
+    const todoItem = {
+      id: this.id,
+      description: txt,
+      status: 'incomplete'
+    }
+    this.todos.push(todoItem)
+    return todoItem
+  }
+  
+  // - Get all todo items
+  getAlltodo(){
+    return this.todos
+  }
 
-function addToDoList(item){
-    toDos.push(item)
-    return toDos
-}
+  // - Set a todo completed by its ID
+  completedTodo(id){
+    const found = todos.find((todo) => todo.id === id)
+    if (!found) return
 
+    found.complete = true
+    return getAlltodo()
+  }
+  // - Get only todo items that are incomplete
+
+  // - Get only todo items that are complete
+
+  // - Search and return a todo item by its ID,
+  // or return a message saying it doesn’t exist
+
+  // - Remove a todo item by its ID
+
+  // getItems() {
+  //   return this.items
+  // }
+
+  // getTodo(filter){
+  //   if (!filter) {
+  //     return allTodo
+  //   }
+  //   if (filter === 'incomplete') {
+  //     const incompleteTodo = this.todos.filter((toDo) => !toDo.completed)
+  //     return incompleteTodo
+  //   }
+  //   if (filter === 'complete') {
+  //     const completeTodo = this.todos.filter((toDo) => toDo.completed)
+  //     return completeTodo
+  //   }
+  // }
+}
 module.exports = {
-    addToDoList
+  TodoList
 }
-console.log(addToDoList("clean"))
