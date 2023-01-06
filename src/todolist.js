a31edbc7eb3a67a2e3297b7c88ac95b78b63bf15
Haroun Anwari <harounanwari@hotmail.co.uk>
2023-01-06 18:12:06 +0000

    complete

--- src/todolist.js
+++ src/todolist.js
@@ -1,1 +1,41 @@
+class TodoList {
+  constructor() {
+    this.id = 0
+    this.items = []
+  }
 
+  create(title) {
+    this.id++
+    const item = { id: this.id, text: title, status: 'incomplete' }
+    this.items.push(item)
+    return item
+  }
+
+  getAllTodos() {
+    return this.items
+  }
+
+  setToComplete(id) {
+    const item = this.findTodo(id)
+    item.status = 'complete'
+    return item
+  }
+
+  getByStatus(status) {
+    return this.items.filter((item) => item.status === status)
+  }
+
+  findTodo(id) {
+    const item = this.items.find((item) => item.id === id)
+    if (item === undefined) throw new Error('Item not found')
+    return item
+  }
+
+  deleteTodoItem(id) {
+    const item = this.findTodo(id)
+    const index = this.items.indexOf(item)
+    return this.items.splice(index, 1)[0]
+  }
+}
+
+module.exports = TodoList
