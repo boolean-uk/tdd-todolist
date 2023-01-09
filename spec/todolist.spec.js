f3468719ad232544a8871ce410ca9c4abc8bf508
akibria12 <ahkibria@hotmail.co.uk>
2023-01-09 14:29:35 +0000

    completed exercise

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,1 +1,102 @@
+const {
+  todo,
+  newTodo,
+  getAllTodos,
+  setCompleted,
+  getIncomplete,
+  getComplete,
+  findTodo,
+  removeTodo
+} = require('../src/todolist.js')
 
+describe('Create a todolist', () => {
+  beforeEach(() => {
+    todo.splice(0, todo.length)
+  })
+  it('should add a task in the list', () => {
+    const updatedTodoList = newTodo('clean the dishes')
+    expect(updatedTodoList).toEqual([
+      {
+        id: 1,
+        description: 'clean the dishes',
+        status: 'incomplete'
+      }
+    ])
+  })
+
+  it('should get all todos', () => {
+    newTodo('clean the dishes')
+    newTodo('mop the floor')
+    const allTodos = getAllTodos()
+    expect(allTodos).toEqual([
+      {
+        id: 1,
+        description: 'clean the dishes',
+        status: 'incomplete'
+      },
+      { id: 2, description: 'mop the floor', status: 'incomplete' }
+    ])
+  })
+
+  it('Set a todo completed by its ID', () => {
+    newTodo('clean the dishes')
+    const updatedTodoList = setCompleted(1)
+    expect(updatedTodoList).toEqual([
+      {
+        id: 1,
+        description: 'clean the dishes',
+        status: 'complete'
+      }
+    ])
+  })
+
+  it('Get only todo items that are incomplete', () => {
+    newTodo('clean the dishes')
+    newTodo('mop the floor')
+    setCompleted(1)
+    const onlyIncompleteStatus = getIncomplete()
+    expect(onlyIncompleteStatus).toEqual([
+      {
+        id: 2,
+        description: 'mop the floor',
+        status: 'incomplete'
+      }
+    ])
+  })
+
+  it('Get only todo items that are complete', () => {
+    newTodo('clean the dishes')
+    newTodo('mop the floor')
+    setCompleted(1)
+    const onlyIncompleteStatus = getComplete()
+    expect(onlyIncompleteStatus).toEqual([
+      {
+        id: 1,
+        description: 'clean the dishes',
+        status: 'complete'
+      }
+    ])
+  })
+
+  it('Search and return a todo item by its ID', () => {
+    newTodo('clean the dishes')
+    newTodo('mop the floor')
+    const found = findTodo(1)
+    expect(found).toEqual({
+      id: 1,
+      description: 'clean the dishes',
+      status: 'incomplete'
+    })
+  })
+  it("return a message saying it doesn't exist", () => {
+    newTodo('clean the dishes')
+    newTodo('mop the floor')
+    const notfound = findTodo(5)
+    expect(notfound).toEqual('No more task to do')
+  })
+
+  it('Remove a todo item by its ID', () => {
+    newTodo('do something')
+    expect(removeTodo(1)).toHaveSize(0)
+  })
+})
--- src/todolist.js
+++ src/todolist.js
@@ -1,1 +1,49 @@
+const todo = []
+function newTodo(description) {
+  todo.push({
+    id: todo.length + 1,
+    description: description,
+    status: 'incomplete'
+  })
+  return todo
+}
 
+function getAllTodos() {
+  return todo
+}
+
+function setCompleted(id) {
+  const findID = todo.findIndex((todo) => todo.id === id)
+  todo[findID].status = 'complete'
+  return todo
+}
+function getIncomplete() {
+  return todo.filter((todo) => todo.status === 'incomplete')
+}
+function getComplete() {
+  return todo.filter((todo) => todo.status === 'complete')
+}
+
+function findTodo(id) {
+  const found = todo.find((todo) => todo.id === id)
+  if (found) {
+    return found
+  }
+  return 'No more task to do'
+}
+function removeTodo(id) {
+  const todoIndex = id - 1
+  todo.splice(todoIndex, 1)
+  return todo
+}
+
+module.exports = {
+  newTodo,
+  todo,
+  getAllTodos,
+  setCompleted,
+  getIncomplete,
+  getComplete,
+  findTodo,
+  removeTodo
+}
