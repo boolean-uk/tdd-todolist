d5bbb9b04cdc8c165fc9b0a82591857df207d988
fatima <fxtimaahmed14@gmail.com>
2023-02-16 13:58:23 +0000

    create task done

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,3 +1,19 @@
 const TodoList = require('../src/TodoList.js')
 
-describe('A to-do list', () => {})
+describe('A to-do list', () => {
+  let ToDoList = TodoList()
+  beforeEach(() => {
+    ToDoList = ToDoList()
+  })
+
+  it('creates a new to-do item on the list', () => {
+    const result = ToDoList.create('do the dishes')
+
+    const expect = {
+      id: 1,
+      task: 'do the dishes',
+      status: 'incomplete'
+    }
+    expect(result).toEqual(expect)
+  })
+})
--- src/todolist.js
+++ src/todolist.js
@@ -3,6 +3,19 @@ class ToDoList {
     this.id = 0
     this.items = []
   }
+
+  create(str) {
+    this.id++
+    const item = { id: this.id, task: str, status: 'incomplete' }
+    this.items.push(item)
+    return item
+  }
+  // create new task
 }
+
+// complete
+// get
+// search
+// remove
 
 module.exports = ToDoList
