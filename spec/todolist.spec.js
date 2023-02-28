f0e752e34927f37ef843f5d1dce822a45a92bae6
Akis-Hajittofi <akis.hajittofi@hotmail.co.uk>
2023-02-28 18:00:10 +0000

    Created test and then source code for createToDo(description)

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,1 +1,11 @@
+const { createToDo } = require('./../src/todolist.js')
 
+describe('To dos', () => {
+  it('Creates a To-Do item', () => {
+    const description = 'Feed the cat'
+    const result = createToDo(description)
+    const expectedResult = { id: 1, text: 'Feed the cat', status: 'incomplete' }
+
+    expect(result).toEqual(expectedResult)
+  })
+})
--- src/todolist.js
+++ src/todolist.js
@@ -1,1 +1,14 @@
+// STATES
+const toDos = []
+let lastUsedId = 0
 
+function createToDo(description) {
+  lastUsedId += 1
+  toDos.push({ id: lastUsedId, text: description, status: 'incomplete' })
+  console.log('TO DO DATA: ', toDos)
+  console.log('toDos LAST ID: ', toDos[lastUsedId])
+
+  return toDos[lastUsedId - 1]
+}
+
+module.exports = { createToDo }
