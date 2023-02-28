4772c219cd9bcde7495219b3cf701e69e5b0cac7
MaybeFreak <michaelpope97@gmail.com>
2023-02-28 17:58:27 +0100

    Completed the Exercise

--- domain-model.md
+++ domain-model.md
@@ -1,1 +1,55 @@
 // Add your domain model below
+
+- Create a todo item that has an ID, text description, and starts off incomplete
+- Get all todo items
+- Set a todo completed by its ID
+- Get only todo items that are incomplete
+- Get only todo items that are complete
+- Search and return a todo item by its ID, or return a message saying it doesn’t exist
+- Remove a todo item by its ID
+
+nouns: todo item, todo items.
+verbs: Get ALL todo items, Set a todo Item to complete by ID, Get incomplete todo items, Get complete todo Items, search and return todo item by ID, remove a todo item by ID.
+todo Item structure: ID, text description, status.
+
+CLASS todoItem(id: @integer, text: @string)
+PROPERTIES
+
+- id: @integer
+- text: @string
+- status: @string
+
+CLASS todoList
+PROPERTIES
+
+- todos: [@todoItem]
+
+METHODS
+
+- createNewTodo(text)
+
+  - OUTPUT: true if successful false if not
+
+- getAllTodos(void)
+
+  - OUTPUT: todos
+
+- getCompletedTodos(void)
+
+  - OUTPUT: [todos.status==='complete']
+
+- getIncompleteTodos(void)
+
+  - OUTPUT: [todos.status==='incomplete']
+
+- getTodoById(id)
+
+  - OUTPUT: todo.id===id : false
+
+- updateTodoStatus(todoItem.id)
+
+  - OUTPUT: true if successful false if not
+
+- removeTodoById(id)
+
+  - OUTPUT: true if successful false if not
--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,1 +1,154 @@
+const { TodoItem, TodoList } = require('../src/todolist.js')
 
+describe('TodoItem should', () => {
+  it('have and id', () => {
+    //SETUP
+    const item = new TodoItem(1, 'test')
+
+    //EXECUTION
+    const expectedRes = 1
+
+    //VERIFY
+    expect(item.id).toEqual(expectedRes)
+  })
+
+  it('should have a text', () => {
+    //SETUP
+    const item = new TodoItem(1, 'test')
+
+    //EXECUTION
+    const expectedRes = 'test'
+
+    //VERIFY
+    expect(item.text).toEqual(expectedRes)
+  })
+
+  it('should have a status of incomplete', () => {
+    //SETUP
+    const item = new TodoItem(1, 'test')
+
+    //EXECUTION
+    const expectedRes = 'incomplete'
+
+    //VERIFY
+    expect(item.status).toEqual(expectedRes)
+  })
+})
+
+describe('TodoList should', () => {
+  it('have todos be an array of TodoItem', () => {
+    //SETUP
+    const items = [new TodoItem(1, 'test')]
+
+    //EXECUTE
+    const expectedRes = [new TodoItem(1, 'test')]
+    const list = new TodoList(items)
+
+    //VERIFY
+    expect(list.todos).toEqual(expectedRes)
+  })
+
+  it('be able to create a new todo and add it to the list', () => {
+    //SETUP
+    const items = [new TodoItem(1, 'test')]
+    const list = new TodoList(items)
+
+    //EXECUTE
+    const expectedRes = [new TodoItem(1, 'test'), new TodoItem(2, 'test')]
+    list.createNewTodo('test')
+
+    //VERIFY
+    expect(list.todos).toEqual(expectedRes)
+  })
+
+  it('be able to get all todos', () => {
+    //SETUP
+    const items = [
+      new TodoItem(1, 'test'),
+      new TodoItem(2, 'test'),
+      new TodoItem(3, 'test')
+    ]
+    const list = new TodoList(items)
+
+    //EXECUTE
+    const expectedRes = [
+      new TodoItem(1, 'test'),
+      new TodoItem(2, 'test'),
+      new TodoItem(3, 'test')
+    ]
+    const res = list.getAllTodos()
+
+    //VERIFY
+    expect(res).toEqual(expectedRes)
+  })
+
+  it('be able to get incomplete todos ', () => {
+    //SETUP
+    const items = [
+      new TodoItem(1, 'test'),
+      new TodoItem(2, 'test'),
+      new TodoItem(3, 'test')
+    ]
+    const list = new TodoList(items)
+
+    //EXECUTE
+    const expectedRes = [new TodoItem(2, 'test'), new TodoItem(3, 'test')]
+    list.updateTodoStatus(1)
+    const res = list.getIncompleteTodos()
+
+    //VERIFY
+    expect(res).toEqual(expectedRes)
+  })
+
+  it('be able to get a todo by id', () => {
+    //SETUP
+    const items = [
+      new TodoItem(1, 'test'),
+      new TodoItem(2, 'test'),
+      new TodoItem(3, 'test')
+    ]
+    const list = new TodoList(items)
+
+    //EXECUTE
+    const expectedRes = new TodoItem(2, 'test')
+    const res = list.getTodoById(2)
+
+    //VERIFY
+    expect(res).toEqual(expectedRes)
+  })
+
+  it('be able to get a todo by id', () => {
+    //SETUP
+    const items = [
+      new TodoItem(1, 'test'),
+      new TodoItem(2, 'test'),
+      new TodoItem(3, 'test')
+    ]
+    const list = new TodoList(items)
+
+    //EXECUTE
+    const expectedRes = 'does not exist'
+    const res = list.getTodoById(4)
+
+    //VERIFY
+    expect(res).toEqual(expectedRes)
+  })
+
+  it('be able to remove a todo by id', () => {
+    //SETUP
+    const items = [
+      new TodoItem(1, 'test'),
+      new TodoItem(2, 'test'),
+      new TodoItem(3, 'test')
+    ]
+    const list = new TodoList(items)
+
+    //EXECUTE
+    const expectedRes = [new TodoItem(1, 'test'), new TodoItem(3, 'test')]
+    list.removeTodoById(2)
+    console.log(list.todos)
+
+    //VERIFY
+    expect(list.todos).toEqual(expectedRes)
+  })
+})
