de4173f7ad01e749d2cf1e5bf0cab11af9ad007d
Kat De Mey <katdemey@outlook.com>
2023-02-28 19:40:10 +0000

    functions written and pass in node.js, 4/5 success in jasmine

--- src/todolist.js
+++ src/todolist.js
@@ -1,1 +1,57 @@
+const todoList = [
+    { id: 0, text: "create test codes", completed: true },
+    { id: 1, text: "test the test codes", completed: false }
+]
 
+const createTodos = (id, text) => {
+    const todo = {
+        id: id,
+        text: text,
+        completed: false,
+    };
+    return todoList.push(todo)
+}
+
+function getAllTodos() {
+    return todoList
+}
+
+
+let incompleteTodoList = []
+
+function getIncompleteTodos() {
+    // return todoList
+    for (let i = 0; i < todoList.length; i++) {
+        if (todoList[i].completed === false) {
+            incompleteTodoList.push(todoList[i])
+        }
+    }
+    return incompleteTodoList
+}
+
+let completeTodoList = []
+function getCompleteTodos() {
+    // return todoList
+    for (let i = 0; i < todoList.length; i++) {
+        if (todoList[i].completed === true) {
+            completeTodoList.push(todoList[i])
+        }
+    }
+    return completeTodoList
+}
+
+function toggleCompleted(id) {
+    const target = todoList.find((todo) => todo.id === id)
+    target.completed === false ?
+        (target.completed = true)
+        :
+        (target.completed = false)
+}
+
+module.exports = {
+    createTodos,
+    getAllTodos,
+    getIncompleteTodos,
+    getCompleteTodos,
+    toggleCompleted
+}
