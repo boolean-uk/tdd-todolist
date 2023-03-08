ce368b25232925179352c28edd9bba8ef1411ffe
Asiyeyurtkuran <asiyeyurtkuran@gmail.com>
2023-03-08 16:01:31 +0300

    Asiye -  core done

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,1 +1,91 @@
+const toDoList = require('../src/todolist.js');
 
+describe("Basket", () => {
+    let todoList
+
+    beforeEach(() => {
+        todoList = new toDoList()
+    })
+
+    it("create an item", () => {
+        //set up
+        const expected = { id: 1, text: "Do the laundry", status: "incomplete" }
+        //execute
+        const result = todoList.createItem("Do the laundry")
+        //verify
+        expect(result).toEqual(expected)
+    })
+
+    it("show all items", () => {
+        //set up
+        const expected = [
+            { id: 1, text: "Do the laundry", status: "incomplete" },
+            { id: 2, text: "Make the bed", status: "incomplete" },
+        ]
+        //execute
+        todoList.createItem("Do the laundry")
+        todoList.createItem("Make the bed")
+        //verify
+        expect(todoList.getItems()).toEqual(expected)
+    })
+
+    it("set completed ", () => {
+        //set up
+        const item = todoList.createItem("Do the laundry")
+        const expected = { id: 1, text: "Do the laundry", status: "complete" }
+        //execute
+        const result = todoList.setComplete(item.id)
+        //verify
+        expect(result).toEqual(expected)
+    })
+
+    it("show complete items", () => {
+        //set up
+        const item1 = todoList.createItem("Do the laundry")
+        const item2 = todoList.createItem("Make the bed")
+        todoList.setComplete(item1.id)
+
+        const expected = [
+            { id: 1, text: "Do the laundry", status: "complete" }
+        ]
+        //execute
+        const result = todoList.getByStatus("complete")
+        //verify
+        expect(result).toEqual(expected)
+    })
+
+    it("find items by Id", () => {
+        //set up
+        const item1 = todoList.createItem("Do the laundry")
+
+        const expected = { id: 1, text: "Do the laundry", status: "incomplete" }
+
+        //execute
+        const result = todoList.findBy(item1.id)
+        //verify
+        expect(result).toEqual(expected)
+    })
+
+    it("throw error if item does not exist", () => {
+        // setup
+        todoList.createItem('Do the laundry')
+        todoList.createItem('Make the bed')
+        // execute
+        const result = todoList.findBy(3)
+        // verify
+        expect(result).toEqual('Item not found')
+    })
+
+
+    it("delete an item", () => {
+        // setup
+        todoList.createItem('Do the laundry')
+        todoList.createItem('Make the bed')
+        todoList.deleteBy(1)
+        // execute
+        const expected = [{id:2, text:"Make the bed", status: "incomplete"}]
+        const result = todoList.getItems()
+        // verify
+        expect(result).toEqual(expected)
+    })
+})
--- src/todolist.js
+++ src/todolist.js
@@ -1,1 +1,44 @@
+class toDoList {
+    constructor() {
+        this.id = 0
+        this.items = []
+    }
+
+    createItem(title) {
+        this.id++
+        const item = { id: this.id, text: title, status: 'incomplete' }
+        this.items.push(item)
+        return item
+    }
+
+    getItems() {
+        return this.items
+    }
+
+    setComplete(id) {
+        const item = this.findBy(id)
+        item.status = 'complete'
+        return item
+    }
+    getByStatus(status) {
+        return this.items.filter(item => item.status === status)
+    }
+
+    findBy(id) {
+        if (this.items[id - 1]) {
+            return this.items[id - 1]
+          }
+          return 'Item not found'
+        }
+    
+
+    deleteBy(id) {
+        const item = this.findBy(id)
+        const index = this.items.indexOf(item)
+        return this.items.splice(index, 1)[0]
+    }
+}
+
+module.exports = toDoList
+
 
