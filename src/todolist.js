1d01ac4dd2690e97beb9113d8046561aa657c81f
Asiyeyurtkuran <asiyeyurtkuran@gmail.com>
2023-04-27 12:15:45 +0100

    missing part added and fixed

--- src/todolist.js
+++ src/todolist.js
@@ -1,44 +1,41 @@
 class toDoList {
-    constructor() {
-        this.id = 0
-        this.items = []
-    }
+  constructor() {
+    this.id = 0
+    this.items = []
+  }
 
-    createItem(title) {
-        this.id++
-        const item = { id: this.id, text: title, status: 'incomplete' }
-        this.items.push(item)
-        return item
-    }
+  createItem(title) {
+    this.id++
+    const item = { id: this.id, text: title, status: 'incomplete' }
+    this.items.push(item)
+    return item
+  }
 
-    getItems() {
-        return this.items
-    }
+  getItems() {
+    return this.items
+  }
 
-    setComplete(id) {
-        const item = this.findBy(id)
-        item.status = 'complete'
-        return item
-    }
-    getByStatus(status) {
-        return this.items.filter(item => item.status === status)
-    }
+  setComplete(id) {
+    const item = this.findBy(id)
+    item.status = 'complete'
+    return item
+  }
 
-    findBy(id) {
-        if (this.items[id - 1]) {
-            return this.items[id - 1]
-          }
-          return 'Item not found'
-        }
-    
+  getByStatus(status) {
+    return this.items.filter((item) => item.status === status)
+  }
 
-    deleteBy(id) {
-        const item = this.findBy(id)
-        const index = this.items.indexOf(item)
-        return this.items.splice(index, 1)[0]
-    }
+  findBy(id) {
+    const item = this.item.find((item) => item.id === id)
+    if (this.items === undefined) throw new Error('Item not found')
+    return item
+  }
+
+  deleteBy(id) {
+    const item = this.findBy(id)
+    const index = this.items.indexOf(item)
+    return this.items.splice(index, 1)[0]
+  }
 }
 
 module.exports = toDoList
-
-
