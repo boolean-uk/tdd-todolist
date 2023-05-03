ddb2a07f5810500d5a3c0df25ddc44bb0597c485
Akis-Hajittofi <akis.hajittofi@hotmail.co.uk>
2023-05-03 15:28:06 +0100

    Tests and source code done for createToDo, getAllToDos and for setting item as complete

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,11 +1,47 @@
-const { createToDo } = require('./../src/todolist.js')
+const {
+  createToDo,
+  getAllToDos,
+  clearToDos,
+  toggleToDo
+} = require('./../src/todolist.js')
 
 describe('To dos', () => {
+  beforeEach(() => {
+    clearToDos()
+  })
+
   it('Creates a To-Do item', () => {
     const description = 'Feed the cat'
     const result = createToDo(description)
     const expectedResult = { id: 1, text: 'Feed the cat', status: 'incomplete' }
 
     expect(result).toEqual(expectedResult)
+  })
+
+  it('gets all To-Do items', () => {
+    createToDo('Feed the cat')
+    createToDo('Feed the dog')
+
+    const expectedToDos = [
+      { id: 1, text: 'Feed the cat', status: 'incomplete' },
+      { id: 2, text: 'Feed the dog', status: 'incomplete' }
+    ]
+
+    const result = getAllToDos()
+    expect(result).toEqual(expectedToDos)
+  })
+
+  it('Sets To-Do to status complete', () => {
+    createToDo('Feed the cat')
+    createToDo('Feed the dog')
+
+    const result = toggleToDo(1)
+
+    const expectedToDos = [
+      { id: 1, text: 'Feed the cat', status: 'incomplete' },
+      { id: 2, text: 'Feed the dog', status: 'complete' }
+    ]
+    expect(getAllToDos()).toEqual(expectedToDos)
+    expect(result).toEqual(expectedToDos[1])
   })
 })
--- src/todolist.js
+++ src/todolist.js
@@ -1,14 +1,30 @@
 // STATES
-const toDos = []
+let toDos = []
 let lastUsedId = 0
 
 function createToDo(description) {
   lastUsedId += 1
   toDos.push({ id: lastUsedId, text: description, status: 'incomplete' })
-  console.log('TO DO DATA: ', toDos)
-  console.log('toDos LAST ID: ', toDos[lastUsedId])
+  // console.log('TO DO DATA: ', toDos)
+  // console.log('toDos LAST ID: ', toDos[lastUsedId])
 
   return toDos[lastUsedId - 1]
 }
 
-module.exports = { createToDo }
+function getAllToDos() {
+  return toDos
+}
+
+function clearToDos() {
+  toDos = []
+  lastUsedId = 0
+}
+
+function toggleToDo(id) {
+  if (toDos[id].status === 'incomplete') {
+    toDos[id].status = 'complete'
+  }
+  return toDos[id]
+}
+
+module.exports = { createToDo, getAllToDos, clearToDos, toggleToDo }
