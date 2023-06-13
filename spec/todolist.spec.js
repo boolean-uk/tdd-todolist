2ee263fa26af809dd1b2a58294c43628d03454f2
Mantas Navickis <navickis@gmail.com>
2023-06-13 17:01:14 +0200

    work in progress

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,18 +1,51 @@
 // import here
-
+const { TodoList } = require('../src/todolist.js')
 
 describe('TodoList', () => {
   it('Creates a new task with the user putting in a string', () => {
     // Given
     const newTodoList = new TodoList()
-    const task = "make the bed"
+    const task = 'make the bed'
     // When
-    const result = toDoList.create(task)
+    const result = newTodoList.create(task)
     // Then
-    expect(result) = {
+    expect(result).toEqual = {
       id: 1,
-      text: "make the bed",
-      status: "incomplete"
+      text: 'make the bed',
+      status: 'incomplete'
+    }
+  })
+  it('Get all ToDos', () => {
+    // Given
+    const newTodoList = new TodoList()
+    const task1 = 'make the bed'
+    const task2 = 'cook dinner'
+    // When
+    newTodoList.create(task1)
+    newTodoList.create(task2)
+    const result = newTodoList.getAll()
+    // Then
+    expect(result).toEqual = [
+      {
+        id: 1,
+        text: 'make the bed',
+        status: 'incomplete'
+      },
+      { id: 2, text: 'cook dinner', status: 'incomplete' }
+    ]
+  })
+  it('Set ToDo list items to complete', () => {
+    // Given
+    const newTodoList = new TodoList()
+    const task = 'make the bed'
+    // When
+    newTodoList.create(task)
+    const result = newTodoList.setComplete(1)
+    // Then
+    expect(result).toEqual = {
+      id: 1,
+      text: 'make the bed',
+      status: 'complete'
     }
   })
 })
--- src/todolist.js
+++ src/todolist.js
@@ -1,11 +1,27 @@
 class TodoList {
-    constructor() {[]}
-    
-    create(todo) {
-        return {            
-        }
+  constructor() {
+    this.toDoArray = []
+  }
+
+  create(todo) {
+    const toDoItem = {
+      id: this.toDoArray.length + 1,
+      text: todo,
+      status: 'incomplete'
     }
-    
+    this.toDoArray.push(toDoItem)
+    return toDoItem
+  }
 
+  getAll() {
+    return this.toDoArray
+  }
 
+  setComplete(id) {
+    const newArray = this.toDoArray.filter((item) => item.id === id)
+    const target = newArray[0]
+    target.status = 'complete'
+    return target
+  }
 }
+module.exports = { TodoList }
