acce693778ddf34ec56b77abad26d9e33d106298
desokan <thefdmm@gmail.com>
2023-06-13 18:56:44 +0100

    completed the create() method

--- src/todolist.js
+++ src/todolist.js
@@ -1,1 +1,37 @@
+class TodoList {
+  constructor() {
+    this.id = 0
+    this.status = 'incomplete'
+    this.todoList = []
+  }
 
+  create(description) {
+    // update the id value.
+    this.id++
+
+    const newTodo = {
+      id: this.id,
+      description: description,
+      status: this.status
+    }
+
+    this.todoList.push(newTodo)
+    return newTodo
+  }
+
+  getAll() {
+    const allTodoList = this.todoList.map((el) => {
+        return 
+    })
+  }
+}
+
+const myTodoList = new TodoList() // Create a new instance of TodoList
+myTodoList.create('Wash the dishes')
+myTodoList.create('Complete the article')
+myTodoList.create('Go to the shop')
+myTodoList.create('Finish the code')
+myTodoList.create('Test the code')
+console.log(myTodoList.todoList)
+
+export default TodoList
