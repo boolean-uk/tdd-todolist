1566d47e09808707415147fa2a06aa0a98d36196
rockettdev <kjr.dev@hotmail.com>
2023-06-13 15:53:03 +0100

    implemented createTodo test and function

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,1 +1,23 @@
+import createTask from "../src/todolist.js"
+import data from "../src/data.js"
 
+describe ("Create Todo", () => {
+
+    it("Return item with ID, name and provided description", () => {
+
+        // setup
+        const taskName = "write tests"
+        const taskId = 1
+        const taskDescription = "Stores List of tests"
+        const taskComplete = false
+
+        // execute
+        createTask(taskName, taskId, taskDescription, taskComplete)
+
+        // verify
+        expect(data[0]).toEqual({taskName: "write tests", taskId: 1, taskDescription: "Stores List of tests", taskComplete: false})
+
+    })
+})
+
+describe ()
--- src/todolist.js
+++ src/todolist.js
@@ -1,1 +1,13 @@
+import data from "./data.js"
 
+const createTask = (taskName, taskId, taskDescription, taskComplete) => {
+
+    data.push({taskName: taskName, taskId: taskId, taskDescription: taskDescription, taskComplete: false})
+
+}
+
+createTask("write tests", 1, "Stores List of tests", false)
+
+console.log(data)
+
+export default createTask
