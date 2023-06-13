693b9bc6ad3e2e6d9bb27a27d0ec116845f24585
Lee Kelly <leeowenkelly@gmail.com>
2023-06-13 16:12:59 +0100

    exercise started

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,1 +1,14 @@
+import Todolist from '../src/todolist'
 
+describe("to do list", () => {
+    const todoList = new Todolist([])
+    it("create todo item", function() {
+        expect(todoList.createTodoItem(1, "finish the exercise")).toEqual({id: 1, text: "finish the exercise", status: "incomplete"})
+    })
+    it("get all todo items", function() {
+        expect(todoList.getAll().toEqual({id: 1, text: "finish the exercise", status: "incomplete"}))
+    })
+    it("set todo complete by id", function() {
+        expect(todoList.setComplete(1).toEqual({id: 1, text: "finish the exercise", status: "complete"}))
+    })
+})
--- src/todolist.js
+++ src/todolist.js
@@ -1,1 +1,12 @@
+class Todolist {
+  constructor(id, text) {
+    this.id = id
+    this.text = text
+  }
 
+  createToDoItem(id, text) {
+    return { id: id, text: text, status: 'incomplete' }
+  }
+}
+
+export default Todolist
