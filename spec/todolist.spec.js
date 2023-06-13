507e9cb938e7453b7c5c5018537cd1f44e69c78a
marc-alzapiedi <marcalzapiedi@gmail.com>
2023-06-13 23:05:08 +0200

    work in progress

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,1 +1,57 @@
+const { create, get, setDone, getIncomplete, toDoList } = require('../src/todolist.js')
 
+describe('create', () => {
+  it('add a new todo to the todo object with a unique id and incomplete status', () => {
+    // Given
+    const todo = 'This string represents a note that is added'
+    // When
+    const result = create(todo)
+
+    // Then
+
+    expect(result).toEqual([{ id: 1, text: todo, status: 'incomplete' }])
+  })
+})
+
+describe('get', () => {
+  it('Return the array with the objects named toDoList', () => {
+    // When
+    const result = get()
+
+    // Then
+    expect(result).toEqual()
+  })
+})
+
+describe('setDone', () => {
+  it('will change the todo by the id to complete', () => {
+    // Given
+    const id = 1
+
+    // When
+    const result = setDone(id)
+
+    // Then
+    expect(result).toEqual([
+      {
+        id: 1,
+        text: 'This string represents a note that is added',
+        status: 'complete'
+      }
+    ])
+  })
+})
+
+describe('getIncomplete', () => {
+  it('return the array of objects that have an incomplete status only', () => {
+    // Given
+    const incompleteList = toDoList.filter((object) => {
+      return object.status === 'incomplete'
+    })
+    // When
+    const result = getIncomplete()
+
+    // Then
+    expect(result).toEqual(incompleteList)
+  })
+})
--- src/todolist.js
+++ src/todolist.js
@@ -1,1 +1,36 @@
+const toDoList = []
+let id = 1
+const create = (todo) => {
+  const obj = {
+    id: id++,
+    text: todo,
+    status: 'incomplete'
+  }
 
+  toDoList.push(obj)
+
+  return toDoList
+}
+
+const get = () => {
+  return toDoList
+}
+
+const setDone = (id) => {
+  const arr = toDoList.filter((object) => {
+    return object.id === id
+  })
+
+  arr[0].status = 'complete'
+  toDoList.splice(toDoList.indexOf(arr[0]), 1, arr[0])
+
+  return toDoList
+}
+
+const getIncomplete = () => {
+  toDoList.filter((object) => {
+    return object.status === 'incomplete'
+  })
+}
+
+module.exports = { create, get, setDone, getIncomplete, toDoList }
