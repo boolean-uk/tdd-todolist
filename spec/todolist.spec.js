9ecba79f32978ce3f51d915dab8de2618670671e
Ian Grantham <dev.iangrantham@gmail.com>
2023-06-13 16:46:18 +0100

    getAll and completeById done

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,23 +1,56 @@
-import createTask from "../src/todolist.js"
-import data from "../src/data.js"
+import { createTask } from '../src/todolist.js'
+import data from '../src/data.js'
+import { getAll } from '../src/todolist.js'
 
-describe ("Create Todo", () => {
+describe('Create Todo', () => {
+  it('Return item with ID, name, provided description and complete set to false', () => {
+    // setup
+    const taskName = 'write tests'
+    const taskId = 1
+    const taskDescription = 'Stores List of tests'
+    const taskComplete = false
 
-    it("Return item with ID, name and provided description", () => {
+    // execute
+    createTask(taskName, taskId, taskDescription, taskComplete)
 
-        // setup
-        const taskName = "write tests"
-        const taskId = 1
-        const taskDescription = "Stores List of tests"
-        const taskComplete = false
-
-        // execute
-        createTask(taskName, taskId, taskDescription, taskComplete)
-
-        // verify
-        expect(data[0]).toEqual({taskName: "write tests", taskId: 1, taskDescription: "Stores List of tests", taskComplete: false})
-
+    // verify
+    expect(data[0]).toEqual({
+      taskName: 'write tests',
+      taskId: 1,
+      taskDescription: 'Stores List of tests',
+      taskComplete: false
     })
+  })
 })
 
-describe ()
+describe('Get all', () => {
+  it('Return all of the task objects in the data array'),
+    () => {
+      //setup
+
+      //execute
+      const res = getAll(data)
+
+      //verify
+      expect(res).toEqual(data)
+    }
+})
+
+describe('complete by ID', () => {
+  it('return the item with the complete set to true'),
+    () => {
+      //setup
+      const taskId = 1
+
+      //execute
+      const res = completeById(taskId)
+
+      //verify
+      expect(res).toEqual({
+        taskName: 'write tests',
+        taskId: 1,
+        taskDescription: 'Stores List of tests',
+        taskComplete: true
+      })
+    }
+})
--- src/todolist.js
+++ src/todolist.js
@@ -1,13 +1,35 @@
-import data from "./data.js"
+import data from './data.js'
+
+//FUNCTIONS
 
 const createTask = (taskName, taskId, taskDescription, taskComplete) => {
-
-    data.push({taskName: taskName, taskId: taskId, taskDescription: taskDescription, taskComplete: false})
-
+  data.push({
+    taskName: taskName,
+    taskId: taskId,
+    taskDescription: taskDescription,
+    taskComplete: false
+  })
 }
 
-createTask("write tests", 1, "Stores List of tests", false)
+const getAll = (data) => {
+  console.log('getAll', data)
+}
 
-console.log(data)
+const completeById = (givenTaskId) => {
+  data.find((item) => {
+    if (item.taskId === givenTaskId) {
+      item.taskComplete = true
+      console.log('completeById', item)
+    }
+  })
+}
 
-export default createTask
+//CALL
+
+createTask('write tests', 1, 'Stores List of tests', false)
+createTask('write tests', 2, 'Stores List of tests', false)
+
+getAll(data)
+completeById(1)
+
+export { createTask, getAll, completeById }
