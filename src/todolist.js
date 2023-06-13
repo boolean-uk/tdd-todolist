73166df545ba8dfcb67d932b6e935845b4296bbb
desokan <thefdmm@gmail.com>
2023-06-13 20:30:52 +0100

    completed getAll(), getTodoById(), setComplete()

--- src/todolist.js
+++ src/todolist.js
@@ -5,7 +5,7 @@ class TodoList {
     this.todoList = []
   }
 
-  create(description) {
+  create(description, id = 0, status = 'incomplete') {
     // update the id value.
     this.id++
 
@@ -20,18 +20,39 @@ class TodoList {
   }
 
   getAll() {
-    const allTodoList = this.todoList.map((el) => {
-        return 
-    })
+    return this.todoList
+  }
+
+  setComplete(id) {
+    const todo = this.getTodoById(id)
+    if (todo) {
+      todo.status = 'complete'
+      return true
+    } else {
+      return false
+    }
+  }
+
+  getTodoById(id) {
+    return this.todoList.find((todo) => todo.id === id)
+  }
+
+  delete(id) {
+    const index = this.todoList.findIndex((todo) => todo.id === id)
+    if (index !== -1) {
+      this.todoList.splice(index, 1)
+      return true
+    } else {
+      return false
+    }
   }
 }
 
 const myTodoList = new TodoList() // Create a new instance of TodoList
 myTodoList.create('Wash the dishes')
 myTodoList.create('Complete the article')
-myTodoList.create('Go to the shop')
-myTodoList.create('Finish the code')
-myTodoList.create('Test the code')
 console.log(myTodoList.todoList)
+
+myTodoList.getAll()
 
 export default TodoList
