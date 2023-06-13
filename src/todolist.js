2701f6404fe48d55b39fb682cfe7eed4659ee110
carolina arruda <carolinacalouroarruda@gmail.com>
2023-06-13 17:46:49 +0200

    first commit

--- src/todolist.js
+++ src/todolist.js
@@ -1,1 +1,29 @@
+class CreateNewTodo {
+  constructor(description) {
+    this.description = description
+    this.id = Math.random()
+    this.status = 'incomplete'
+  }
 
+//   create(newTodo) {
+//     return this.todoList.push(newTodo)
+//   }
+}
+
+class TodoList {
+constructor(todoItems) {
+    this.todoItems = []
+}
+
+create(description) {
+    const newTodo = new CreateNewTodo(description)
+
+}
+}
+
+const newTodo = new CreateNewTodo('study')
+console.log(newTodo)
+
+
+
+export default CreateNewTodo
