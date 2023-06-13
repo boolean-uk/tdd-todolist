de791ea131c63b5859a91c6230e47f5ba21f37a3
carolina arruda <carolinacalouroarruda@gmail.com>
2023-06-13 20:02:26 +0200

    finished tests

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -48,4 +48,59 @@ describe('Create Todo List', () => {
       }
     ])
   })
+  it('returns item not found', () => {
+    td.createNewTodo('study')
+    td.createNewTodo('laundry')
+    td.setTodoComplete(3)
+    expect(td.setTodoComplete(3)).toEqual(
+      'The todo your looking for was not found'
+    )
+  })
+  it('returns only incomplete', () => {
+    td.createNewTodo('study')
+    td.createNewTodo('laundry')
+    td.setTodoComplete(2)
+    expect(td.getAlltodoIncompleteItems()).toEqual([
+      {
+        id: 1,
+        description: 'study',
+        status: 'incomplete'
+      }
+    ])
+  })
+  it('returns only complete', () => {
+    td.createNewTodo('study')
+    td.createNewTodo('laundry')
+    td.setTodoComplete(2)
+    expect(td.getAlltodoCompleteItems()).toEqual([
+      {
+        id: 2,
+        description: 'laundry',
+        status: 'complete'
+      }
+    ])
+  })
+  it('returns item searched', () => {
+    td.createNewTodo('study')
+    td.createNewTodo('laundry')
+    expect(td.searchByToDoId(2)).toEqual([
+      {
+        id: 2,
+        description: 'laundry',
+        status: 'incomplete'
+      }
+    ])
+  })
+  it('delete a todo with id', () => {
+    td.createNewTodo('study')
+    td.createNewTodo('laundry')
+    td.deleteTodo(1)
+    expect(td.getAllTodoItems()).toEqual([
+      {
+        id: 2,
+        description: 'laundry',
+        status: 'incomplete'
+      }
+    ])
+  })
 })
