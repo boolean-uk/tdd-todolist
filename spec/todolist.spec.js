fd31b09a7c4cfaf9582070a7bbd1bd9c261a2fd6
Sara Beazley <sarajbeazley@gmail.com>
2023-06-14 12:32:22 -0700

    1st requirement done

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,1 +1,19 @@
+import TodoList from '../src/todolist.js'
 
+describe('Create Todo', () => {
+  it('should add a new todo item to the list with the provided description as well as a unique id', () => {
+    // SET UP
+    const todoList = new TodoList()
+    const expected = {
+      id: 1,
+      description: 'go shopping',
+      isComplete: false
+    }
+
+    // EXECUTE
+    const result = todoList.addItem('go shopping')
+
+    // VERIFY
+    expect(result).toEqual(expected)
+  })
+})
--- src/todolist.js
+++ src/todolist.js
@@ -1,1 +1,21 @@
+class TodoList {
+  constructor() {
+    this.todoList = []
+  }
+
+  addItem(description) {
+    const toDo = {
+      id: this.todoList.length + 1,
+      description: description,
+      isComplete: false
+    }
+    return toDo
+  }
+
+//   getAll
+
+}
+
+
+export default TodoList
 
