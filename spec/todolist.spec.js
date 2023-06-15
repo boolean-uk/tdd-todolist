c63633e6aa19970970a6ff7ae26753c7f87d48b6
Sara Beazley <sarajbeazley@gmail.com>
2023-06-15 13:26:15 -0700

    completed core

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,19 +1,180 @@
 import TodoList from '../src/todolist.js'
 
-describe('Create Todo', () => {
-  it('should add a new todo item to the list with the provided description as well as a unique id', () => {
-    // SET UP
-    const todoList = new TodoList()
-    const expected = {
-      id: 1,
-      description: 'go shopping',
-      isComplete: false
-    }
+describe('TodoList', () => {
+  describe('addItem', () => {
+    it('should add a new todo item to the list with the provided description as well as a unique id', () => {
+      // SET UP
+      //   getting everything in place before you run the function that you are testing
+      const todoList = new TodoList()
+      const expected = [
+        {
+          id: 1,
+          description: 'go shopping',
+          isComplete: false
+        }
+      ]
+      // EXECUTE
+      const result = todoList.addItem('go shopping')
 
-    // EXECUTE
-    const result = todoList.addItem('go shopping')
-
-    // VERIFY
-    expect(result).toEqual(expected)
+      // VERIFY
+      expect(result).toEqual(expected)
+    })
   })
+
+  describe('getAll', () => {
+    it('should get all todo items from the list', () => {
+      // SET UP
+      const todoList = new TodoList()
+      todoList.addItem('go shopping')
+      todoList.addItem('buy milk')
+      const expected = [
+        {
+          id: 1,
+          description: 'go shopping',
+          isComplete: false
+        },
+        {
+          id: 2,
+          description: 'buy milk',
+          isComplete: false
+        }
+      ]
+      // EXECUTE
+      const result = todoList.getAll()
+
+      // VERIFY
+      expect(result).toEqual(expected)
+    })
+  })
+
+  describe('setComplete', () => {
+    it('set todo completed by id', () => {
+      // SET UP
+      const todoList = new TodoList()
+      todoList.addItem('go shopping')
+      const expected = {
+        id: 1,
+        description: 'go shopping',
+        isComplete: true
+      }
+
+      // EXECUTE
+      const result = todoList.setComplete(1)
+
+      // VERIFY
+      expect(result).toEqual(expected)
+    })
+  })
+
+  describe('getIncomplete', () => {
+    it('get to do items that are incomplete', () => {
+      // SET UP
+      const todoList = new TodoList()
+      todoList.addItem('go shopping')
+      const expected = [
+        {
+          id: 1,
+          description: 'go shopping',
+          isComplete: false
+        }
+      ]
+      // EXECUTE
+      const result = todoList.getIncomplete()
+
+      // VERIFY
+      expect(result).toEqual(expected)
+    })
+  })
+
+  describe('getComplete', () => {
+    it('get to do items that are complete', () => {
+      // SET UP
+      const todoList = new TodoList()
+      todoList.addItem('go shopping')
+      todoList.setComplete(1)
+      const expected = [
+        {
+          id: 1,
+          description: 'go shopping',
+          isComplete: true
+        }
+      ]
+      // EXECUTE
+      const result = todoList.getComplete()
+
+      // VERIFY
+      expect(result).toEqual(expected)
+    })
+  })
+
+  describe('searchByid', () => {
+    it('search for an item by its id', () => {
+      // SET UP
+      const todoList = new TodoList()
+      todoList.addItem('go shopping')
+      todoList.addItem('buy milk')
+      const expected = {
+        id: 2,
+        description: 'buy milk',
+        isComplete: false
+      }
+
+      // EXECUTE
+      const result = todoList.searchByid(2)
+
+      // VERIFY
+      expect(result).toEqual(expected)
+    })
+  })
+
+  describe('searchByid, unhappy path', () => {
+    it('search for an item by its id', () => {
+      // SET UP
+      const todoList = new TodoList()
+      todoList.addItem('go shopping')
+      todoList.addItem('buy milk')
+      const expected = 'item does not exist'
+
+      // EXECUTE
+      const result = todoList.searchByid(6)
+
+      // VERIFY
+      expect(result).toEqual(expected)
+    })
+  })
+
+  describe('removeByid', () => {
+    it('remove item by its id', () => {
+      // SET UP
+      const todoList = new TodoList()
+      todoList.addItem('go shopping')
+      todoList.addItem('buy milk')
+      const expected = [
+        {
+          id: 2,
+          description: 'buy milk',
+          isComplete: false
+        }
+      ]
+      //   EXECUTE
+      const result = todoList.removeByid(1)
+      // VERIFY
+      expect(result).toEqual(expected)
+    })
+  })
+
+  describe('removeByid, unhappy path', () => {
+    it('remove item by its id', () => {
+      // SET UP
+      const todoList = new TodoList()
+      todoList.addItem('go shopping')
+      todoList.addItem('buy milk')
+      const expected = 'no match found'
+      //   EXECUTE
+      const result = todoList.removeByid(4)
+      // VERIFY
+      expect(result).toEqual(expected)
+    })
+  })
+
 })
--- src/todolist.js
+++ src/todolist.js
@@ -1,21 +1,73 @@
 class TodoList {
   constructor() {
+    this.id = 0
     this.todoList = []
   }
 
   addItem(description) {
     const toDo = {
-      id: this.todoList.length + 1,
+      id: ++this.id,
       description: description,
       isComplete: false
     }
-    return toDo
+    this.todoList.push(toDo)
+    return this.todoList
   }
 
-//   getAll
+  getAll() {
+    return this.todoList
+  }
 
+  setComplete(id) {
+    const toDo = this.todoList.find((item) => item.id === id)
+    if (toDo.isComplete === false) {
+      toDo.isComplete = true
+      return toDo
+    }
+  }
+
+  getIncomplete() {
+    const incomplete = this.todoList.map((item) => {
+      if (item.isComplete === false) {
+        return item
+      }
+    })
+    return incomplete
+  }
+
+  getComplete() {
+    const complete = this.todoList.map((item) => {
+      if (item.isComplete === true) {
+        return item
+      }
+    })
+    return complete
+  }
+
+  searchByid(id) {
+    const toDo = this.todoList.find((item) => {
+      if (item.id === id) {
+        console.log(item)
+        return item
+      }
+    })
+    if (toDo) {
+      return toDo
+    } else return 'item does not exist'
+  }
+
+  removeByid(id) {
+    const toDoIndex = this.todoList.findIndex((item) => item.id === id)
+    // console.log(toDoIndex)
+
+    if (toDoIndex !== -1) {
+      this.todoList.splice(toDoIndex, 1)
+    } else {
+      return 'no match found'
+    }
+
+    return this.todoList
+  }
 }
 
-
 export default TodoList
-
