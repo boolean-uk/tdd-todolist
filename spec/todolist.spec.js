b72bea0891b638921ba58c97a4b11dc0e4732087
marc-alzapiedi <marcalzapiedi@gmail.com>
2023-06-17 11:36:32 +0200

    Finished core

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,11 +1,3 @@
-// const { create, get, setDone, getIncomplete } = require('../src/todolist.js')
-
-// create('This is a note')
-// create('This is a second note')
-// create('This is a third note')
-// create('This is a fourth note')
-// create('This is a fifth note')
-
 describe('todoListFunctions', () => {
   beforeEach(() => {
     const cache = require.cache
@@ -16,6 +8,7 @@ describe('todoListFunctions', () => {
 
   describe('create', () => {
     it('add a new todo to the todo object with a unique id and incomplete status', () => {
+      // Set up
       const { create } = require('../src/todolist.js')
       // Given
       const todo =
@@ -31,11 +24,12 @@ describe('todoListFunctions', () => {
   })
 
   describe('get', () => {
-    it('Is going to return the array with objects (the todos)', () => {
+    it('It is going to return the array with objects (the todos)', () => {
+      // set up
       const { get, create } = require('../src/todolist.js')
+      create('This is a note')
 
       // When
-      create('This is a note')
       const result = get()
 
       // Then
@@ -52,13 +46,14 @@ describe('todoListFunctions', () => {
 
   describe('setDone', () => {
     it('Will identify the todo by id and change the status to complete', () => {
+      // Set up
       const { setDone, create } = require('../src/todolist.js')
+      create('This is a note')
 
       // Given
       const id = 1
 
       // When
-      create('This is a note')
       const result = setDone(id)
 
       // Then
@@ -72,11 +67,12 @@ describe('todoListFunctions', () => {
 
   describe('getIncomplete', () => {
     it('Will return the array of objects that have an incomplete status only', () => {
+      // Set up
       const { getIncomplete, create, setDone } = require('../src/todolist.js')
-
-      // When
       create('This is some text that represents a note that has been added')
       create('This is a third note that has been added')
+
+      // When
       setDone(1)
 
       const result = getIncomplete()
@@ -88,6 +84,103 @@ describe('todoListFunctions', () => {
           text: 'This is a third note that has been added',
           status: 'incomplete'
         }
+      ])
+    })
+  })
+
+  describe('getComplete', () => {
+    it('Will return the array of objects that have an complete status only', () => {
+      // Set up
+      const { getComplete, create, setDone } = require('../src/todolist.js')
+      create('This is some text that represents a note that has been added')
+      create('This is another note that has been added')
+      create('This is another note that has been added')
+
+      // When
+      setDone(2)
+      setDone(3)
+
+      const result = getComplete()
+
+      // Then
+      expect(result).toEqual([
+        {
+          id: 2,
+          text: 'This is another note that has been added',
+          status: 'complete'
+        },
+        {
+          id: 3,
+          text: 'This is another note that has been added',
+          status: 'complete'
+        }
+      ])
+    })
+  })
+
+  describe('getById', () => {
+    // Happy path
+    it('Will search the todo array and return the object with the corresponding id passed through the function', () => {
+      // set up
+      const { getById, create, setDone } = require('../src/todolist.js')
+      create('This is a note')
+      create('THis is another note')
+      create('This is a third note')
+      create('This is a fourth note')
+      create('This is a fifth note')
+      setDone(3)
+      setDone(4)
+
+      // When
+      const result = getById(5)
+
+      // Then
+      expect(result).toEqual({
+        id: 5,
+        text: 'This is a fifth note',
+        status: 'incomplete'
+      })
+    })
+
+    // Sad path
+    it('Will return a message saying that the todo does not exist if there is no corresponding id in the todo array', () => {
+      // Set up
+      const { getById, create, setDone } = require('../src/todolist.js')
+      create('This is a note')
+      create('THis is another note')
+      create('This is a third note')
+      create('This is a fourth note')
+      create('This is a fifth note')
+      setDone(3)
+      setDone(4)
+
+      // When
+      const result = getById(10)
+
+      // Then
+      expect(result).toEqual('The todo does not exist')
+    })
+  })
+
+  describe('delete', () => {
+    it('Will search to todo array and delete the todo that has the corresponding id and return the new updated array', () => {
+      // Set up
+      const { create, deleteTodo } = require('../src/todolist.js')
+      create('This is a note')
+      create('This is a second note')
+      create('This is a third note')
+      create('This is a fourth note')
+      create('This is a fifth note')
+
+      // When
+      const result = deleteTodo(4)
+
+      // Then
+      expect(result).toEqual([
+        { id: 1, text: 'This is a note', status: 'incomplete' },
+        { id: 2, text: 'This is a second note', status: 'incomplete' },
+        { id: 3, text: 'This is a third note', status: 'incomplete' },
+        { id: 5, text: 'This is a fifth note', status: 'incomplete' }
       ])
     })
   })
--- src/todolist.js
+++ src/todolist.js
@@ -36,4 +36,41 @@ const getIncomplete = () => {
   return incompleteArr
 }
 
-module.exports = { create, get, setDone, getIncomplete }
+const getComplete = () => {
+  const completeArr = toDoList.filter((object) => {
+    return object.status === 'complete'
+  })
+
+  return completeArr
+}
+
+const getById = (id) => {
+  const arr = toDoList.filter((object) => {
+    return object.id === id
+  })
+
+  if (arr[0] === undefined) {
+    return 'The todo does not exist'
+  }
+  return arr[0]
+}
+
+const deleteTodo = (id) => {
+  const arr = toDoList.filter((object) => {
+    return object.id === id
+  })
+
+  toDoList.splice(toDoList.indexOf(arr[0]), 1)
+
+  return toDoList
+}
+
+module.exports = {
+  create,
+  get,
+  setDone,
+  getIncomplete,
+  getComplete,
+  getById,
+  deleteTodo
+}
