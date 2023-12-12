6704f1bf8eb987542d14758f6d7520e0b754ded2
Rebecca Noy <rebecca.noy@gmx.de>
2023-12-12 16:51:37 +0100

    initialized project

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,1 +1,14 @@
+const { createToDo } = '../src/todolist'
 
+console.log(createToDo)
+describe('create todos', () => {
+  console.log(createToDo)
+  it('description is valid', () => {
+    const expectedResult = {
+      description: 'Test',
+      complete: false
+    }
+
+    expect(createToDo('Test')).toBe(expectedResult)
+  })
+})
--- src/todolist.js
+++ src/todolist.js
@@ -1,1 +1,6 @@
+const createToDo = (description) => {
+  if (!description) return false
+  return { description, complete: false }
+}
 
+export { createToDo }
