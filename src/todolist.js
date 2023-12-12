ec68a4885274a35f6ac24d8340c69b9081aefe93
aayushlama4008 <aayushlama4008@gmail.com>
2023-12-12 18:20:05 +0100

    created function todolist

--- src/todolist.js
+++ src/todolist.js
@@ -1,1 +1,37 @@
+const toDoList = [
+  {
+    id: 1,
+    description: 'mow the grass',
+    complete: false
+  },
+  {
+    id: 2,
+    description: 'do the dishes',
+    complete: false
+  },
+  {
+    id: 3,
+    description: 'go to the gym',
+    complete: false
+  }
+]
 
+const createToDo = (input) => {
+  const num = toDoList.length + 1
+
+  const toDoItem = {
+    id: num,
+    description: input,
+    complete: false
+  }
+
+  console.log(toDoItem)
+  if (typeof input === 'string') {
+    return toDoItem
+  } else return false
+}
+
+createToDo()
+module.exports = {
+  createToDo
+}
