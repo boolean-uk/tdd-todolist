58adb4034cd66879512059683cbced3d17886639
Lukas Dembicki <lad_777@hotmail.com>
2023-12-12 16:17:25 +0000

    first test done.

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,1 +1,26 @@
+describe(' Todo Functions', () => {
+  describe('Create Todo', () => {
+    it('if there is a valid string', () => {
+      // GIVEN
+      const str = 'Do laundry'
+      // WHEN
+      const item1 = {
+        id: 1,
+        description: 'Do laundry',
+        completed: false
+      }
+      const result = createTodo(str)
+      // THEN
+      expect(result).toEqual(item1)
+    })
 
+    it('if there is not a valid string', () => {
+      // GIVEN
+      const str = ''
+      // WHEN
+      const result = createTodo(str)
+      // THEN
+      expect(result).toEqual(false)
+    })
+  })
+})
