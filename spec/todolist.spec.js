77b66541236430566535d2cf5febc48be8dcac59
Satoki <satokiito@gmail.com>
2023-12-12 17:03:54 +0000

    added second test to make sure text is a string when new todo is created

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,9 +1,15 @@
 const { todoList, createTodo } = require('../src/todolist.js')
 
 describe('Todo List', () => {
-  it('return new todo if data entered', () => {
+  it('returns new todo item as an object', () => {
     const text = 'new todo'
     const newToDo = createTodo(text)
     expect(newToDo).toEqual({ id: 1, text: 'new todo', status: 'incomplete' })
+  })
+
+  it('user can only enter a string as the todo text description', () => {
+    const text = 1
+    const newToDo = createTodo(text)
+    expect(newToDo).toEqual('text value must be a valid string')
   })
 })
--- src/todolist.js
+++ src/todolist.js
@@ -1,13 +1,17 @@
 const todoList = []
 
 const createTodo = (text) => {
-  const newToDo = {
-    id: todoList.length + 1,
-    text,
-    status: 'incomplete'
+  if (typeof text !== 'string') {
+    return 'text value must be a valid string'
+  } else {
+    const newToDo = {
+      id: todoList.length + 1,
+      text,
+      status: 'incomplete'
+    }
+    todoList.push(newToDo)
+    return newToDo
   }
-  todoList.push(newToDo)
-  return newToDo
 }
 
 // console.log(createTodo('hi'))
