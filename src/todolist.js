a9d0ae5f49645ab9e4f74df2f1af72053e6ee8a9
Atif Furqaan <atifurqaan@gmail.com>
2023-12-12 13:38:02 +0000

    Completed the core

--- src/todolist.js
+++ src/todolist.js
@@ -1,1 +1,47 @@
-
+const toDoList = []
+function createToDo(id, description, status) {
+  const toDoItem = {
+    id: id,
+    description: description,
+    status: status
+  }
+  toDoList.push(toDoItem)
+  return toDoItem
+}
+function getAll(toDoList) {
+  if (toDoList.length === 0) {
+    return "no todo's"
+  } else {
+    return toDoList
+  }
+}
+function setStatus(id, toDoList) {
+  const todoItem = toDoList.find((x) => x.id === id)
+  todoItem.status = 'complete'
+  return todoItem
+}
+function getIncomplete(toDoList) {
+  const filteredList = toDoList.filter((x) => x.status === 'incomplete')
+  return filteredList
+}
+function getComplete(toDoList) {
+  const filteredList = toDoList.filter((x) => x.status === 'complete')
+  return filteredList
+}
+function findTodo(id, toDoList) {
+  const todoItem = toDoList.find((x) => x.id === id)
+  return todoItem
+}
+function removeItem(id, todoList) {
+  const filteredList = todoList.filter((x) => x.id !== id)
+  return filteredList
+}
+module.exports = {
+  createToDo,
+  getAll,
+  setStatus,
+  getIncomplete,
+  getComplete,
+  findTodo,
+  removeItem
+}
