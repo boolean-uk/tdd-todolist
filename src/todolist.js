1a4bdebe855a47b803f5ab5c159ec469fea25c7e
Rebecca Noy <rebecca.noy@gmx.de>
2023-12-13 07:38:46 +0100

    reload list when user adds new item

--- src/todolist.js
+++ src/todolist.js
@@ -1,3 +1,42 @@
+const init = () => {
+  const button = document.getElementById('add')
+  const input = document.querySelector('input')
+
+  const reaction = () => {
+    console.log(input.value)
+    console.log('click!')
+  }
+
+  button.addEventListener('click', reaction())
+}
+
+const loadList = () => {
+  const ul = document.querySelector('ul#todos')
+  const entries = document.querySelectorAll('ul#todos li')
+
+  entries.forEach((entry) => entry.remove())
+
+  const todos = myList.getAll()
+
+  todos.forEach((todo) => {
+    const checkbox = document.createElement('input')
+    checkbox.setAttribute('type', 'checkbox')
+    if (todo.complete) {
+      checkbox.checked = true
+    }
+
+    const li = document.createElement('li')
+    li.innerText = todo.description
+    li.appendChild(checkbox)
+
+    if (todo.complete) {
+      li.setAttribute('class', 'strikethrough')
+    }
+
+    ul.appendChild(li)
+  })
+}
+
 class TodoList {
   constructor(title) {
     this.title = title || 'untitled'
@@ -12,6 +51,7 @@ class TodoList {
     if (!description) return false
     const newItem = { id: this.items.length + 1, description, complete: false }
     this.items.push(newItem)
+    loadList()
     return newItem
   }
 
@@ -37,45 +77,6 @@ const myList = new TodoList('My List')
 myList.create('Test entry')
 myList.create("Test entry 2 but it's done")
 myList.setComplete(2)
-
-const handleSubmit = (event) => {
-  console.log('hello')
-}
-
-const init = () => {
-  const button = document.getElementById('add')
-  const input = document.querySelector('input')
-
-  const reaction = () => {
-    console.log(input.value)
-    console.log('click!')
-  }
-
-  button.addEventListener('click', reaction())
-}
-
-const loadList = () => {
-  const ul = document.querySelector('ul#todos')
-  const todos = myList.getAll()
-
-  todos.forEach((todo) => {
-    const checkbox = document.createElement('input')
-    checkbox.setAttribute('type', 'checkbox')
-    if (todo.complete) {
-      checkbox.checked = true
-    }
-
-    const li = document.createElement('li')
-    li.innerText = todo.description
-    li.appendChild(checkbox)
-
-    if (todo.complete) {
-      li.setAttribute("class", "strikethrough")
-    }
-
-    ul.appendChild(li)
-  })
-}
 
 init()
 loadList()
