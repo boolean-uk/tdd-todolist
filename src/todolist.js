2471734d4886e8a09111a27980e4b42482c54990
Nazar Tymiv <nazartumiv@gmail.com>
2023-12-13 01:57:29 +0000

    TodoList class created :)

--- src/todolist.js
+++ src/todolist.js
@@ -1,1 +1,23 @@
+class TodoList {
+  constructor() {
+    this.todoList = []
+  }
 
+  createTodo() {}
+
+  getAllTodo() {}
+
+  setTodoComplete() {}
+
+  getIncompleteTodo() {}
+
+  getCompletedTodo() {}
+
+  searchTodo() {}
+
+  removeTodo() {}
+}
+
+module.exports = {
+  TodoList
+}
