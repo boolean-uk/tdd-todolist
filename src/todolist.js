8ab9e9c71607482691682efb9f4ef58f9d7593f3
Jack Miller <jdmiller1991@outlook.com>
2023-12-18 19:31:01 +0000

    Core complete

--- src/todolist.js
+++ src/todolist.js
@@ -1,1 +1,53 @@
+class TodoList {
+  constructor() {
+    this.todos = []
+    this.currentId = 1
+  }
 
+  createTodo(description) {
+    const todo = {
+      id: this.currentId++,
+      description,
+      completed: false
+    }
+    this.todos.push(todo)
+    return todo
+  }
+
+  getAllTodos() {
+    return this.todos
+  }
+
+  getTodoById(id) {
+    return (
+      this.todos.find((todo) => todo.id === id) || 'Todo item does not exist'
+    )
+  }
+
+  setTodoCompleted(id) {
+    const todo = this.getTodoById(id)
+    if (todo !== 'Todo item does not exist') {
+      todo.completed = true
+    }
+    return todo
+  }
+
+  getIncompleteTodos() {
+    return this.todos.filter((todo) => !todo.completed)
+  }
+
+  getCompleteTodos() {
+    return this.todos.filter((todo) => todo.completed)
+  }
+
+  removeTodoById(id) {
+    const index = this.todos.findIndex((todo) => todo.id === id)
+    if (index !== -1) {
+      this.todos.splice(index, 1)
+      return true
+    }
+    return false
+  }
+}
+
+module.exports = TodoList
