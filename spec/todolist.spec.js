1b13b7788c87a292b7619502a62122f2b0823e40
Tayokanch <tayoquadri11@gmail.com>
2024-01-01 11:39:48 +0000

    refactoring my test and code

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,78 +1,67 @@
-import Todolist from '../src/todolist'
+const {Todolist, existingTodoList}= require('../src/todolist.js')
+
+
+
 
 describe('TDD todoList', () => {
-  const todos = [
-    {
-      id: 1,
-      text: 'I will be going to the gym',
-      status: 'incomplete'
-    },
-    {
-      id: 2,
-      text: 'I am going for a date',
-      status: 'incomplete'
-    }
-  ]
-  const todoListInstance = new Todolist(todos)
+  const todoItemListInstance = new Todolist()
 
+
+describe('Can create a new todo', ()=>{
   it('creates a todo item with an ID, text description, and incomplete status', () => {
+  
     const newTodoText = 'I am traveling'
-    const result = todoListInstance.create(newTodoText)
-
-    const newId = todoListInstance.todolist.length + 1
-
+    const newId = todoItemListInstance.todolist.length + 1
+  
+    const result = todoItemListInstance.create(newTodoText)
+  
+    expect(result).toEqual(this.todolists.length - 1)
+  
     expect(result).toEqual({
       id: newId,
       text: newTodoText,
       status: 'incomplete'
     })
   })
+})
 
-  it('Get all todo items', () => {
-    const result = todoListInstance.getAllTodo(todos)
-    expect(result).toBeTrue()
-  })
 
-  it('set a todo completed by its ID', () => {
-    const result = todoListInstance.setComplete(todos)
-    expect(result).toBe({
-      id: 2,
-      text: 'I am going for a date',
-      status: 'complete'
+  describe('Can see all the existing todoList',()=>{
+
+    it('Get all todo items', () => {
+      const result = todoItemListInstance.getAllTodo()
+      expect(result).toBeTrue()
     })
   })
 
-  it('get todo items that are incomplete', () => {
-    const allIncompleteTodo = todos.filter(
-      (todo) => todo.status === 'incomplete'
-    )
-    const result = todoListInstance.getIncompleteTodos()
+  describe('Can change the status of a todo by its ID',()=>{
 
-    expect(result).toEqual(allIncompleteTodo)
+    it('set a todo status to completed by its ID', () => {
+      const result = todoItemListInstance.setComplete(1)
+      expect(result).toEqual('status changed')
+    })
+  
+    it('set a todo completed by its ID', () => {
+      const result = todoItemListInstance.setComplete(4)
+      expect(result).toEqual('Todo not found')
+    })
   })
 
-  it('get todo items that are complete', () => {
-    const allCompleteTodo = todos.filter((todo) => todo.status === 'complete')
-    const result = todoListInstance.getCompleteTodos()
 
-    expect(result).toEqual(allCompleteTodo)
+  describe('Can get all todos with incomplete status', ()=>{
+
+    it('get todo items that are incomplete', () => {
+      const allIncompleteTodo = existingTodoList.filter(
+        (todo) => todo.status === 'incomplete'
+      )
+      const result = todoItemListInstance.getIncompleteTodos()
+  
+      expect(result).toEqual(allIncompleteTodo)
+    })
   })
 
-  it('Search and return a todo item by its ID, or return a message saying it doesn’t exist', () => {
-    const todoId = 2
-    const searchTodowithId = todos.find((todo) => todo.id === todoId)
-    const searchResult = searchTodowithId || 'doesnt exist'
-    const result = todoListInstance.searchTodoById(searchResult)
-    expect(result).toEqual(searchResult)
-  })
 
-  it('Remove a todo item by its ID', () => {
-    const idTobeRemoved = 2
 
-    const removeTodowithId = todos.filter((todo) => todo.id !== idTobeRemoved)
 
-    const result = todoListInstance.removeById()
 
-    expect(result).toEqual(removeTodowithId)
-  })
 })
--- src/todolist.js
+++ src/todolist.js
@@ -1,4 +1,4 @@
-const todoList = [
+const existingTodoList = [
   {
     id: 1,
     text: 'I will be going to the gym',
@@ -8,101 +8,59 @@ const todoList = [
     id: 2,
     text: 'I am going for a date',
     status: 'incomplete'
+  },
+
+  {
+    id: 3,
+    text: 'I am going for new year eve event',
+    status: 'complete'
   }
 ]
 
 class Todolist {
-  constructor(todolist) {
-    this.todolist = todolist
+  constructor() {
+    this.todolists = existingTodoList;
   }
-
   create(newTodo) {
     const newId =
-      this.todolist.length > 0
-        ? this.todolist[this.todolist.length - 1].id + 1
+      this.todolists.length > 0
+        ? this.todolists[this.todolists.length - 1].id + 1
         : 1
 
-    this.todolist.push({
+    this.todolists.push({
       id: newId,
       text: newTodo,
       status: 'incomplete'
     })
 
-    return this.todolist
+    return this.todolists
   }
 
   getAllTodo() {
-    return this.todolist
+    return this.todolists.length > 0 ? true : false;
   }
 
   setComplete(todoId) {
-    this.todolist.forEach((todo) => {
-      if (todo.id === todoId) {
-        todo.status = 'complete'
-      }
-    })
+    const index = this.todolists.findIndex((todo) => todo.id === todoId);
 
-    return this.todolist
+    if (index !== -1) {
+      this.todolists[index].status = 'complete';
+      return 'status changed';
+    } else {
+      return 'Todo not found';
+    }
   }
 
   getIncompleteTodos() {
-    return this.todolist.filter((todo) => todo.status === 'incomplete')
-  }
-
-  getCompleteTodos() {
-    return this.todolist.filter((todo) => todo.status === 'complete')
-  }
-
-  searchTodoById() {
-    const searchId = 4
-    const seachById = this.todolist.find((todo) => todo.id === searchId)
-    const result = seachById || ` Todo with Id ${searchId} doesn't exist`
-
-    return result
-  }
-
-  /*     removeById(){
-        const idTobeRemoved = 2
-
-        const removeTodowithId = this.todolist.filter(todo=> todo.id !== idTobeRemoved)
-        return removeTodowithId
-    } */
-
-  removeById() {
-    const idTobeRemoved = 2
-
-    this.todolist = this.todolist.filter((todo) => todo.id !== idTobeRemoved)
-    return this.todolist
+    return this.todolists.filter((todo) => todo.status === 'incomplete')
   }
 }
 
-const todoInstance = new Todolist(todoList)
-const todo3 = todoInstance.create('i dey go home')
-const todo4 = todoInstance.create('I am going for shopping')
-const todo5 = todoInstance.create('I need to do my assignment')
-/* console.log(todo5) */ console.log('==============================')
-const alltodolist = todoInstance.getAllTodo()
-console.log(alltodolist)
-console.log('==============================')
 
-const completeTodo = todoInstance.setComplete(2)
-console.log(completeTodo)
-console.log('==============================')
 
-const allIncompleteTodos = todoInstance.getIncompleteTodos()
-console.log('this is allIncompleteTodos', allIncompleteTodos)
-console.log('==============================')
 
-const allCompleteTodos = todoInstance.getCompleteTodos()
-console.log('this is allCompleteTodos', allCompleteTodos)
-console.log('==============================')
 
-const searchTodoWithId = todoInstance.searchTodoById()
-console.log('this is the search by Id', searchTodoWithId)
-console.log('==============================')
-
-const removeTodo = todoInstance.removeById()
-console.log('this is the remaining todo', removeTodo)
-
-/* export default Todolist
- */
+module.exports = {
+  Todolist,
+  existingTodoList
+}
