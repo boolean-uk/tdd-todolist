948c6acba42307cf7b60b248e1483383adcd377b
tomlukrprogram <tomlukeprogramming@outlook.com>
2024-01-13 20:19:37 +0000

    first 2 tests complete

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,1 +1,27 @@
+const { Todo, TodoList } = require('../src/todolist.js')
 
+describe('TodoList', () => {
+  it('should create a todo item with ID, text description, and start off incomplete', () => {
+    const todoList = new TodoList()
+    const todoText = 'Complete TDD test'
+
+    const todo = todoList.create(todoText)
+
+    expect(todo).toBeInstanceOf(Todo)
+    expect(todo.id).toBe(1)
+    expect(todo.text).toBe(todoText)
+    expect(todo.completed).toBe(false)
+  })
+
+  it('should get all todo items', () => {
+    const todoList = new TodoList()
+    const todo1 = todoList.create('Complete TDD test')
+    const todo2 = todoList.create('Implement get all todos')
+
+    const allTodos = todoList.getAll()
+
+    expect(allTodos).toHaveLength(2)
+    expect(allTodos).toContainEqual(todo1)
+    expect(allTodos).toContainEqual(todo2)
+  })
+})
--- src/todolist.js
+++ src/todolist.js
@@ -1,1 +1,29 @@
+class TodoList {
+  constructor() {
+    this.todos = []
+    this.currentId = 1
+  }
 
+  create(text) {
+    const todo = new Todo(this.currentId++, text)
+    this.todos.push(todo)
+    return todo
+  }
+
+  getAll() {
+    return this.todos
+  }
+}
+
+class Todo {
+  constructor(id, text) {
+    this.id = id
+    this.text = text
+    this.completed = false
+  }
+}
+
+module.exports = {
+  Todo,
+  TodoList
+}
