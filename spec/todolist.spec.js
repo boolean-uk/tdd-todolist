917015fdd02537202f1eed39583ec1f76282c58a
Periklis <perik76@gmail.com>
2024-05-30 02:51:16 +0300

    Re-refactor to improve id assignment -Re-refactor tests

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,14 +1,14 @@
 import { ToDoItem, ToDosList } from "../src/todolist.js";
 
 describe('todo', () => {
-    it('Should have a task description, isComplete must be fals and must have an id of 0 for the 1st task and 1 for the 2nd', () => {
-        const todo1 = new ToDoItem('Test_task_1')
-        expect(todo1.id).toBe(0)
+    it('Should have a task description, isComplete must be false and must have an id of 0 for the 1st task and 1 for the 2nd', () => {
+        const todo1 = new ToDoItem(1,'Test_task_1')
+        expect(todo1.id).toBe(1)
         expect(todo1.taskDescr).toBe('Test_task_1')
         expect(todo1.isComplete).toBe(false)
 
-        const todo2 = new ToDoItem('Test_task_2')
-        expect(todo2.id).toBe(1)
+        const todo2 = new ToDoItem(2,'Test_task_2')
+        expect(todo2.id).toBe(2)
         expect(todo2.taskDescr).toBe('Test_task_2')
         expect(todo2.isComplete).toBe(false)        
     })
@@ -16,8 +16,17 @@ describe('todo', () => {
 
 describe('To Dos list', () => {
     it('Should be an array with 0 length', () => {
-        const toDos1 = new ToDosList()
-        expect(toDos1.toDos.length).toBe(0)
+        const toDos = new ToDosList()
+        expect(toDos.toDos.length).toBe(0)
     })
+    
+//     it('Should be an array with 1 length after adding a ToDoItem', () => {
+//         const toDos1 = new ToDosList()
+//         expect(toDos1.toDos.length).toBe(0)
 
+//         const toDo = toDos1.addToDo('test the code')
+//         expect(toDos1.toDos.length).toBe(1)        
+//         expect(toDo.id).toBe(0)
+//         // expect(toDo.taskDescr).toBe('test the code')
+//     })
 })
--- src/todolist.js
+++ src/todolist.js
@@ -1,18 +1,42 @@
 class ToDoItem {
-  static currentId = 0
-  constructor(taskDescr) {
-    this.id = ToDoItem.currentId++
+  constructor(id, taskDescr) {
+    this.id = id
     this.taskDescr = taskDescr
     this.isComplete = false
   }
 }
 
 class ToDosList {
-    constructor() {
-        this.toDos = []
-    }
+  constructor() {
+    this.toDos = []
+  }
 
+  addToDo(taskDescr) {
+    this.toDos.push(new ToDoItem(this.toDos.length + 1, taskDescr))
+    return this.toDos
+  }
 
+  // allTodos()
 }
+
+// const test1  = new ToDosList
+// function testing() {
+//     test1.addToDo('test1')
+//     test1.addToDo('test2')
+//     test1.addToDo('test3')
+//     console.log(test1);
+//     // return test1
+
+// }
+// console.log(testing());
+
+// const test2 = new ToDosList()
+// test2.addToDo('test2')
+// const test3 = new ToDosList
+// test3.addToDo('test3')
+// console.log(test2);
+// console.log(test3);
+// console.log(test1.toDos.length);
+// console.log(test3.toDos.length);
 
 export { ToDoItem, ToDosList }
