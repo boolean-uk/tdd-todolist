fbe3451f1856742897fa243d8a73711acb0faddc
Will Baxter <bax.will@googlemail.com>
2024-05-29 13:21:15 +0100

    Added first test for ToDoItem structure

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,1 +1,9 @@
+import { ToDoItem } from '../src/todolist.js'
+
+describe('To Do List', () => {
+  it('To do item should have ID, text, and be set to incomplete', () => {
+    const item = new ToDoItem(1, 'Learn to code', false)
+    expect(Object.keys(item)).toBe(['id', 'toDo', 'complete'])
+  })
+})
 
