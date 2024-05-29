4b4bd0ebfa9b7a3061384665bb94c1d844544acf
Farshad <f.bagdeli13@gmail.com>
2024-05-29 14:04:47 +0200

    describe todo and todos done

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,1 +1,14 @@
+import { Todos, Todo } from "../src/todolist.js"
+
+describe('todo', () => {
+  it('should have an id and text and status', () => {
+    const todo1 = new Todo(1, 'Finish the Exercise', false)
+  })
+})
+
+describe('todos', () => {
+  it('should have a list of Todos', () => {
+    const todosList = new Todos()
+  })
+})
 
--- src/todolist.js
+++ src/todolist.js
@@ -1,1 +1,14 @@
+class Todo {
+  constructor(id, text, status) {
+    this.id = id
+    this.text = text
+    this.status = status
+  }
+}
+class Todos {
+  constructor() {
+    this.todos = []
+  }
+}
 
+export { Todos, Todo }
