983609481a36fc53adffc148fcaee631bb49c4bc
Tim Zoltie <t.zoltie@gmail.com>
2024-05-29 13:03:09 +0100

    todoitem class created

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,1 +1,10 @@
+import { TodoItem } from '../src/todolist.js'
 
+describe('todoItem', () => {
+  it('should have an id, string, and status', () => {
+    const todoItem = new TodoItem(Number, 'String', Boolean)
+    expect(todoItem.id).toBe(Number)
+    expect(todoItem.todo).toBe('String')
+    expect(todoItem.status).toBe(Boolean)
+  })
+})
--- src/todolist.js
+++ src/todolist.js
@@ -1,1 +1,9 @@
+class TodoItem {
+  constructor(id, todo, status = false) {
+    this.id = id
+    this.todo = todo
+    this.status = status
+  }
+}
 
+export { TodoItem }
