a3e86d65b29bbe75178067a64434f2d1be1da98d
Tim Zoltie <t.zoltie@gmail.com>
2024-05-29 16:35:26 +0100

    setComplete function created

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -2,10 +2,18 @@ import { TodoItem, TodoList } from '../src/todolist.js'
 
 describe('todoItem', () => {
   it('should have an id, string, and status', () => {
-    const todoItem = new TodoItem(Number, 'String', Boolean)
+    const todoItem = new TodoItem(Number, 'String', false)
     expect(todoItem.id).toBe(Number)
     expect(todoItem.todo).toBe('String')
-    expect(todoItem.status).toBe(Boolean)
+    expect(todoItem.status).toBe(false)
+  })
+  it('should change status to complete if todo is set to true', () => {
+    const todoItem = new TodoItem(Number, 'String', true)
+    expect(todoItem.status).toBe(true)
+    const isComplete = new TodoItem(Number, 'String', true)
+    expect(isComplete.status).toBe(true)
+    const completedTodo = todoItem.setComplete(isComplete)
+    expect(completedTodo.status).toBe(true)
   })
 })
 
@@ -14,7 +22,7 @@ describe('todoList', () => {
     const todoList = new TodoList()
     expect(todoList.todos.length).toBe(0)
 
-    const addTodo = new TodoItem(Number, ' ', Boolean)
+    const addTodo = new TodoItem(Number, ' ', false)
     const list = todoList.add(addTodo)
     expect(todoList.todos.length).toBe(1)
     expect(list.length).toBe(1)
--- src/todolist.js
+++ src/todolist.js
@@ -1,8 +1,16 @@
 class TodoItem {
-  constructor(id, todo, status = false) {
+  constructor(id, todo, status) {
     this.id = id
     this.todo = todo
     this.status = status
+  }
+
+  setComplete(status) {
+    this.todo.status = 'incomplete'
+    if (!status) {
+      this.todo.status = 'complete'
+    }
+    return this.todo
   }
 }
 
