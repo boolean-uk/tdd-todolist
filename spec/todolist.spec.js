9212293fa3260dcf593926cd1664f6302c3e6e31
Will Baxter <bax.will@googlemail.com>
2024-05-29 14:13:43 +0100

    Added test for get all toDos

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,8 +1,9 @@
+/* eslint-disable prettier/prettier */
 import { ToDoList } from '../src/todolist.js'
 import 'jasmine-expect'
 
 describe('To Do List', () => {
-  it('To do manager should have a create function that adds a new ToDo and adds an unique id with default complete status set to false', () => {
+  it('To do list should have a create function that adds a new ToDo and adds an unique id with default complete status set to false', () => {
     const toDoList = new ToDoList()
 
     expect(toDoList.create('Learn to code')).toEqual({
@@ -10,5 +11,17 @@ describe('To Do List', () => {
       text: 'Learn to code',
       complete: false
     })
+  })
+
+  it('To do list should be able to return all todo items', () => {
+    const toDoList = new ToDoList()
+
+    toDoList.create('Learn to code')
+    toDoList.create('Learn to code better')
+
+    expect(toDoList.getAllToDos()).toEqual([
+      { id: 1, text: 'Learn to code', complete: false },
+      { id: 2, text: 'Learn to code better', complete: false }
+    ])
   })
 })
