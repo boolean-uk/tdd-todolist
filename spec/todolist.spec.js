12626face0c6b8d8a13a5c0ab03bb1921fffefe6
Angus Townsley <angustownsley@gmail.com>
2024-05-29 13:34:51 +0100

    Add test for createToDo

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -6,4 +6,11 @@ describe('todolist', () => {
 
     expect(toDoList.toDos.length).toBe(0)
   })
+
+  it("creates toDos and adds to list", () => {
+    const toDoList = new ToDoList()
+    toDoList.createToDo('Do This')
+
+    expect(toDoList.toDos[1].text).toBe('Do this')
+  })
 })
