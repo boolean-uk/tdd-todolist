02677629d4912c19da71c9597bb079442f7f5b0d
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-05-29 13:11:26 +0200

    create test to see if item has a description

---
+++ spec/todolist.spec.js
@@ -0,0 +1,8 @@
+const Item = require('../src/todolist.js')
+
+describe('item', () => {
+  it('should have an id, description and be incomplete', () => {
+    const item = new Item('something')
+    expect(item.description).toBe('something')
+  })
+})
