fc3ee92c547a3961cf728a1de0c0658a1b5867d6
Will Baxter <bax.will@googlemail.com>
2024-05-29 13:58:00 +0100

    Wiped all previous progress and restarted

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,38 +1,3 @@
-import { ToDoItem, ToDoManager } from '../src/todolist.js'
+import { ToDoManager } from '../src/todolist.js'
 import 'jasmine-expect'
 
-describe('To Do List', () => {
-  it('To do item should have ID, text, and be set to incomplete', () => {
-    const toDo = new ToDoItem(1, 'Learn to code', false)
-
-    expect(Object.keys(toDo)).toEqual(['id', 'toDo', 'complete'])
-  })
-
-  it('To do manager should have an add item function', () => {
-    const tdm = new ToDoManager()
-
-    expect(tdm.addToDo).toBeFunction()
-  })
-
-  it('To do manager should have an add item function that adds items to the list', () => {
-    const tdm = new ToDoManager()
-    const toDo = new ToDoItem(1, 'Learn to code', false)
-    tdm.addToDo(toDo)
-
-    expect(tdm.toDos.length).toBeGreaterThan(0)
-  })
-
-  it('To do manager should have a get all items function that returns all toDos', () => {
-    const tdm = new ToDoManager()
-    const toDo = new ToDoItem(1, 'Learn to code', false)
-    const toDo2 = new ToDoItem(2, 'Learn to code better', false)
-
-    tdm.addToDo(toDo)
-    tdm.addToDo(toDo2)
-
-    expect(tdm.getAllToDos).toEqual([
-      { id: 1, toDo: 'Learn to code', complete: false },
-      { id: 2, toDo: 'Learn to code better', complete: false }
-    ])
-  })
-})
--- src/todolist.js
+++ src/todolist.js
@@ -1,19 +1,7 @@
-class ToDoItem {
-  constructor(id, toDo, complete) {
-    this.id = id
-    this.toDo = toDo
-    this.complete = complete
-  }
-}
-
 class ToDoManager {
   constructor() {
     this.toDos = []
   }
-
-  addToDo(item) {
-    this.toDos.push(item)
-  }
 }
 
-export { ToDoItem, ToDoManager }
+export { ToDoManager }
