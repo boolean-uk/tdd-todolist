e6030252fd9728cb862e89d04a6367e16b625b10
Angus Townsley <angustownsley@gmail.com>
2024-05-29 13:44:58 +0100

    Implement createToDo function

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,16 +1,16 @@
 import ToDoList from '../src/todolist.js'
 
 describe('todolist', () => {
-  it("exists", () => {
+  it('exists', () => {
     const toDoList = new ToDoList()
 
     expect(toDoList.toDos.length).toBe(0)
   })
 
-  it("creates toDos and adds to list", () => {
+  it('creates toDos and adds to list', () => {
     const toDoList = new ToDoList()
     toDoList.createToDo('Do This')
 
-    expect(toDoList.toDos[1].text).toBe('Do this')
+    expect(toDoList.toDos[0].text).toBe('Do This')
   })
 })
--- src/todolist.js
+++ src/todolist.js
@@ -1,6 +1,19 @@
+import crypto from 'crypto'
+
 class ToDoList {
   constructor() {
     this.toDos = []
+  }
+
+  createToDo(text) {
+    const toDo = {
+      id: crypto.randomUUID(),
+      text,
+      complete: false
+    }
+
+    this.toDos.push(toDo)
+    return toDo
   }
 }
 
