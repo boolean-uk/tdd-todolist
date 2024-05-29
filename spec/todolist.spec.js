bc9dde3e3ad85e6cc48e070179224018cd8a1e4b
Periklis <perik76@gmail.com>
2024-05-30 00:12:16 +0300

    Created and teted ToDoItem

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,1 +1,10 @@
+import { ToDoItem } from "../src/todolist.js";
 
+describe('todo', () => {
+    it('Should have an id, task description and isComplete must be false', () => {
+        const todo = new ToDoItem(1, 'Test task')
+        expect(todo.id).toBe(1)
+        expect(todo.task).toBe('Test task')
+        expect(todo.isComplete).toBe(false)
+    })
+})
--- src/todolist.js
+++ src/todolist.js
@@ -1,1 +1,9 @@
+class ToDoItem {
+  constructor(id, task) {
+    this.id = id
+    this.task = task
+    this.isComplete = false
+  }
+}
 
+export { ToDoItem }
