ad0b4f1696cfbe056ce762c1896e3717b5ae8d45
Angus Townsley <angustownsley@gmail.com>
2024-05-29 13:25:00 +0100

    Add class validation test

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,1 +1,9 @@
+import ToDoList from '../src/todolist.js'
 
+describe('todolist', () => {
+  it("exists", () => {
+    const toDoList = new ToDoList()
+
+    expect(toDoList).toBeInstanceOf(ToDoList)
+  })
+})
