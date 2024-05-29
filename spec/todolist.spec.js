18880ea41b512dabfa90e6b1cca08352530a1919
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-05-29 13:18:06 +0200

    add incomplete expectation to item test

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -3,6 +3,8 @@ const { Item } = require('../src/todolist.js')
 describe('item', () => {
   it('should have an id, description and be incomplete', () => {
     const item = new Item('something')
+
     expect(item.description).toBe('something')
+    expect(item.isCompleted).toBe('incomplete')
   })
 })
