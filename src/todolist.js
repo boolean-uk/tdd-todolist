b4b41df821863ef384b33e55f4df0445f3b1dcfb
Tim Zoltie <t.zoltie@gmail.com>
2024-05-30 18:15:59 +0100

    create todo function created

--- src/todolist.js
+++ src/todolist.js
@@ -1,10 +1,20 @@
 class TodoItem {
-  
+  constructor(id, title, status) {
+    this.id = id
+    this.title = title
+    this.status = status
+  }
 
 
 }
 
 class TodoList {
+
+  createTodo(title) {
+    const todo = new TodoItem(1, title)
+
+    return todo
+  }
   
 }
 
