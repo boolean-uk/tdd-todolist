2e4ec162b731065fc31cbb8f99e91379be0203c7
Tim Zoltie <t.zoltie@gmail.com>
2024-05-30 18:13:59 +0100

    create todo test created

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -2,29 +2,39 @@ import { TodoItem, TodoList } from '../src/todolist.js'
 
 describe('todoItem', () => {
   it('should have an id, string, and status', () => {
-    const todoItem = new TodoItem(Number, 'String', false)
-    expect(todoItem.id).toBe(Number)
-    expect(todoItem.todo).toBe('String')
-    expect(todoItem.status).toBe(false)
+    const todoItem = new TodoItem()
+    expect(todoItem.id).toBe(1)
+    expect(todoItem.todo).toBe('complete todo list')
+    expect(todoItem.status).toBe('incomplete')
   })
   it('should change status to complete if todo is set to true', () => {
-    const todoItem = new TodoItem(Number, 'String', true)
-    expect(todoItem.status).toBe(true)
-    const isComplete = new TodoItem(Number, 'String', true)
-    expect(isComplete.status).toBe(true)
-    const completedTodo = todoItem.setComplete(isComplete)
-    expect(completedTodo.status).toBe(true)
+    const todoItem = new TodoItem(1, 'String', 'incomplete')
+    expect(todoItem.status).toBe('incomplete')
+    const completedTodo = todoItem.setComplete('complete')
+    expect(completedTodo.status).toBe('complete')
   })
 })
 
 describe('todoList', () => {
+  it('should create a new ID for each todo', () => {
+    const todo1 = tod
+  })
   it('should show all todos', () => {
     const todoList = new TodoList()
     expect(todoList.todos.length).toBe(0)
 
-    const addTodo = new TodoItem(Number, ' ', false)
+    const addTodo = new TodoItem(1, ' ', 'incomplete')
     const list = todoList.add(addTodo)
     expect(todoList.todos.length).toBe(1)
     expect(list.length).toBe(1)
+  })
+  it('should get all todos that are incomplete', () => {
+    const incomplete = new TodoList([new TodoItem('incomplete')])
+    expect(incomplete.find('incomplete' || 'complete')).toBeTrue()
+  })
+  it('should find a todo by ID', () => {
+    const getTodo = new TodoList([new TodoItem(1)])
+    expect(getTodo.search(1)).toBeTrue()
+    expect(getTodo.search(undefined)).toBe('Todo item not found')
   })
 })
--- src/todolist.js
+++ src/todolist.js
@@ -1,40 +1,11 @@
 class TodoItem {
-  constructor(id, todo, status) {
-    this.id = id
-    this.todo = todo
-    this.status = status
-  }
+  
 
-  setComplete(status) {
-    this.status = 'incomplete'
-    if (!status) {
-      this.status = 'complete'
-    }
-    return this.status
-  }
+
 }
 
 class TodoList {
-  constructor() {
-    this.todos = []
-  }
-
-  add(todo) {
-    this.todos.push(todo)
-    return this.todos
-  }
-
-  find(status) {
-    return this.todos.find((todo) => todo.status === status)
-  }
-
-  search(id) {
-    const getId = this.todos.find((todo) => todo.id === id)
-    if (getId === undefined) {
-      return 'Todo item not found'
-    }
-    return getId
-  }
+  
 }
 
 export { TodoItem, TodoList }
