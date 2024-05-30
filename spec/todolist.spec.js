f620caccad3f239b27263a9afb3f2d21b7ae404f
Alistair Henshaw <alistairhenshaw@gmail.com>
2024-05-30 15:25:48 +0100

    tests made to make sure todo created has a valid input

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -7,4 +7,12 @@ describe("ToDoList", () => {
         toDoList.createToDo("a task to complete")
         expect(toDoList.getToDoList()).toEqual([{id: 1, task: "a task to complete", complete: false}])
     })
+
+    it("created to do must have at least one character", () => {
+        expect(toDoList.createToDo("")).toEqual("task needs at least one character")
+    })
+
+    it("value for creating a todo must be a string", () => {
+        expect(toDoList.createToDo(23).toEqual("task must be a string"))
+    })
 })
