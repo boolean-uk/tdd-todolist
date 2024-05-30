e11143d7e18c308427b05cf8e85f1f81ffa00da0
Hamada Abdelaal <hamada_abdelaal@hotmail.com>
2024-05-30 16:08:50 +0200

    TodoList should exist

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,1 +1,8 @@
+import TodoList from '../src/todolist.js'
 
+describe('TodoList', () => {
+  it('should exist', () => {
+    const td = new TodoList()
+    expect(td).toBeInstanceOf(TodoList)
+  })
+})
