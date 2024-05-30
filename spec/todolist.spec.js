1dbd702f9c90697e570fb6ae5ca80e5144d83d25
Kyle Vann <kyle.vann1996@icloud.com>
2024-05-30 20:02:41 +0100

    4th test passed

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -9,10 +9,10 @@ describe('ToDoList', () => {
 
   it('should create a task.', () => {
     const taskLength = taskList.tasks.length
-    console.log("text1:", taskList.tasks)
+    console.log('text1:', taskList.tasks)
     taskList.createTask({ id: 1, text: 'Wash armpits', completed: false })
     expect(taskList.tasks.length).toEqual(taskLength + 1)
-    console.log("text2:", taskList.tasks)
+    console.log('text2:', taskList.tasks)
   })
 
   it('should get all tasks.', () => {
@@ -24,5 +24,12 @@ describe('ToDoList', () => {
     const firstTask = taskList.tasks[0]
     expect(firstTask.completed).toEqual(false)
     taskList.completeTask(firstTask.id)
+  })
+
+  it('should get incompleted tasks.', () => {
+    const incompleteTasks = taskList.tasks.filter((item) => {
+      return item.completed === false
+    })
+    expect(taskList.getIncompleteTasks()).toEqual(incompleteTasks)
   })
 })
--- src/todolist.js
+++ src/todolist.js
@@ -16,6 +16,12 @@ class ToDoList {
       return { ...item, completed: item.id === taskId ? true : item.completed }
     })
   }
+
+  getIncompleteTasks() {
+    return this.tasks.filter((item) => {
+      return item.completed === false
+    })
+  }
 }
 
 module.exports = ToDoList
