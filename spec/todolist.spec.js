ef4eb996201fcfcddad5e51aebe5914c820c6500
Alistair Henshaw <alistairhenshaw@gmail.com>
2024-05-30 16:38:50 +0100

    set complete added to class

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,14 +1,18 @@
 import ToDoList from "../src/todolist.js"
 
 describe("ToDoList", () => {
-    const toDoList = new ToDoList()
+    let toDoList 
+
+    beforeEach(() => {
+        toDoList = new ToDoList()
+    })
 
     it("be able to create a to do task", () => {
         toDoList.createToDo("a task to complete")
         expect(toDoList.getToDoList()).toEqual([{id: 1, task: "a task to complete", complete: false}])
     })
 
-    it("created to do must have at least one character", () => {
+    it("created to do must have at least one character", () => {    
         expect(toDoList.createToDo("")).toEqual("task needs at least one character")
     })
 
--- src/todolist.js
+++ src/todolist.js
@@ -16,6 +16,18 @@ class ToDoList {
     getToDoList() {
         return this.todos
     }
+    setComplete(id) {
+        let task = undefined
+        for (let i = 0; i < this.todos.length; i++) {
+            if (this.todos[i].id === id) {
+                task = this.todos[i]
+                task.complete = true
+            }
+        }
+        if (task === undefined) {
+            return "to do id doesn't exist"
+        }
+    }
 }
 
 // const test = new ToDoList ()
