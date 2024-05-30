64648eaecbb98e409e152935a49e21c155339354
Alistair Henshaw <alistairhenshaw@gmail.com>
2024-05-30 14:17:03 +0100

    spec check a to do can be created

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,1 +1,11 @@
+// import ToDoList from '../src/todolist.js'
 
+describe("ToDo", () => {
+    it("be able to create a to do task", () => {
+        const toDoList = new ToDoList()
+
+        toDoList.createToDo("a task to complete")
+
+        expect(todoList.getToDoList().toBe([{id: 1, task: "a task to complete", complete: false}]))
+    })
+})
