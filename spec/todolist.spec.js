3e389d787de3e9e646e1846601340b6879f34a76
Shaun Harris <shaun@shaunwilliamharris.com>
2024-05-31 16:02:44 +0100

    initial set up of entities and addItem - Failed

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,1 +1,19 @@
+import { TodoList, TodoItem} from "../src/todolist.js"
 
+describe('TodoList', () => {
+    let todoList
+
+    beforeEach(() => {
+        todoList = new TodoList()
+    })
+
+    it('should add a new todo item', () => {
+        const item = todoList.addTodoItem('1', 'TDD')
+        expect(item).toBeInstanceOf(TodoItem)
+        expect(item.id).toBe('1')
+        expect(item.description).toBe('Test')
+        expect(item.isCompleted).toBe(false)
+        expect(todoList.getAllItems()).toContain(item)
+    })
+
+})
--- src/todolist.js
+++ src/todolist.js
@@ -1,1 +1,25 @@
+class TodoItem {
+    constructor(id,description) {
+        this.id =id
+        this.description = description
+        this.isCompleted = false
+    }
+}
 
+class TodoList {
+    constructor() {
+        this.items = []
+    }
+
+    addTodoItem(id, description) {
+        const newItem = new TodoItem(id, description)
+        this.items.push(newItem)
+        return newItem
+    }
+
+    getAllItems() {
+        return this.items
+    }
+}
+
+export { TodoItem, TodoList }
