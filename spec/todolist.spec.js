a30075c4500a7f79c9013a7ac384a968b24609f8
Hamada Abdelaal <hamada_abdelaal@hotmail.com>
2024-05-31 11:47:40 +0200

    should create a new todo-n

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,8 +1,20 @@
 import TodoList from '../src/todolist.js'
 
 describe('TodoList', () => {
-  it('should exist', () => {
-    const td = new TodoList()
-    expect(td).toBeInstanceOf(TodoList)
+  let todoList
+  beforeEach(() => {
+    todoList = new TodoList()
   })
+
+  it('should exist', () => {
+    expect(todoList).toBeInstanceOf(TodoList)
+  })
+})
+
+it('should create a new todo', () => {
+  const todo = todoList.create('make tea')
+
+  expect(todo.id).toBe(1)
+  expect(todo.text).toBe('make tea')
+  expect(todo.isComplete).toBe(false)
 })
