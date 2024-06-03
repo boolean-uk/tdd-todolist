13d42a2cb197d60b852c6563a1785db79d5d9823
JOANNABUUMA1 <nabuumaj6@gmail.com>
2024-06-03 13:10:05 +0100

    expectation for multiple todos with incrementing ids test

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,23 +1,33 @@
 import TodoList, { Todo } from "../src/todolist.js"
 
 describe("To doList", () => {
-    let todolist
+    let todoList
     beforeEach(() => {
-        todolist = new TodoList()
+        todoList = new TodoList()
 
     })
 
 it("should exist", () => {
-    expect(todolist).toBeInstanceOf(TodoList)
+    expect(todoList).toBeInstanceOf(TodoList)
    })
 
    it("should create a new todo", () => {
-       const result = todolist.create('complete exercise')
+       const result = todoList.create('complete exercise')
 
        expect(result).toBeInstanceOf(Todo)
        expect(result.id).toBe(1)
        expect(result.text).toBe('complete exercise')
        expect(result.status).toBe('incomplete')
+
+  })
+
+  it("should create multiple todos with incrementing IDs", () => {
+    const result1 = todoList.create('complete exercise')
+    const result2 = todoList.create('work out')
+
+    expect(result1.id).toBe(1)
+    expect(result1.text).toBe('complete exercise')
+    expect(result1.status).toBe('incomplete')
 
   })
 })
