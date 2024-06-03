85755a1a7b5ac50abb8d08fb188a0e7373a95b25
Rafa Lopes <rafalopessecond@gmail.com>
2024-06-03 10:07:56 +0100

    create todo item tests

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,1 +1,9 @@
+const TodoItem = require('../src/todoitem')
+describe('TodoItem', () => {
+  it('Should initialize ID and status properties', () => {
+    const todoItem = new TodoItem('A task')
 
+    expect(todoItem?.ID).toBeDefined()
+    expect(todoItem?.status).toBeFalsy()
+  })
+})
