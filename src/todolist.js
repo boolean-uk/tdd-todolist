dd23a2038c223c299be24b9c64378b70bcd369a7
samisalehsaeed <samisalehsaeed05@gmail.com>
2024-06-04 14:10:13 +0100

    Sami Saeed

--- src/todolist.js
+++ src/todolist.js
@@ -1,1 +1,50 @@
+class Todo {
+  constructor(id, description) {
+    this.id = id
+    this.description = description
+    this.completed = false
+  }
+}
 
+class TodoList {
+  constructor() {
+    this.items = []
+    this.currentId = 1
+  }
+
+  addTodo(description) {
+    const todo = new Todo(this.currentId++, description)
+    this.items.push(todo)
+    return todo
+  }
+
+  getTodos() {
+    return this.items
+  }
+
+  setTodoCompleted(id) {
+    const todo = this.items.find((item) => item.id === id)
+    if (todo) {
+      todo.completed = true
+    }
+  }
+
+  getIncompleteTodos() {
+    return this.items.filter((item) => !item.completed)
+  }
+
+  getCompleteTodos() {
+    return this.items.filter((item) => item.completed)
+  }
+
+  searchTodoById(id) {
+    const todo = this.items.find((item) => item.id === id)
+    return todo || 'Todo item not found'
+  }
+
+  removeTodoById(id) {
+    this.items = this.items.filter((item) => item.id !== id)
+  }
+}
+
+module.exports = { TodoList }
